72684e6eb9917bc3e9508c541ccfeb9f49971446
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-04-22 11:11:32 +0100

    callenge completed

--- index.java
+++ index.java
@@ -1,10 +1,10 @@
-// Write a Java program to print the sum of two numbers
+// Write a Java program that takes two numbers as input and display the product of two numbers.
 public class index {
     
     public static void main(String args[]) {
-        int a = 10;
+        int a = 50;
         int b = 30;
-        int result = b + a;
+        int result = b * a;
         System.out.println(result);
     }
 }
