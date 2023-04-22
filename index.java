e3ebaa42c4fd06b9a517b1dae26574479093083c
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-04-22 11:04:46 +0100

    fixes

--- index.java
+++ index.java
@@ -4,7 +4,7 @@ public class index {
     public static void main(String args[]) {
         int a = 10;
         int b = 30;
-        int result = b / a;
+        int result = b + a;
         System.out.println(result);
     }
 }
