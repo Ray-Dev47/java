7575e07668b5bab28efe87d47471af71fc13c5ff
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-04-22 10:56:57 +0100

    completed

--- index.java
+++ index.java
@@ -1,12 +1,15 @@
-// import javax.naming.spi.DirStateFactory.Result;
+// Write a Java program to print the result of the following operations. Go to the editor
+// Test Data:
 
-// Write a Java program to print 'Hello' on screen and then print your name on a separate line
 public class index {
-    
     public static void main(String args[]) {
-        int a = 10;
-        int b = 30;
-        int result = b / a;
-        System.out.println(result);
+        int a = -5 + 8 * 6;
+        int b = (55+9) % 9;
+        int c = 20 + -3 * 5 / 7;
+        int d = 5 + 15 / 3 * 2 - 8 % 3;
+         System.out.println(a);
+         System.out.println(b);
+         System.out.println(c);
+         System.out.println(d);
     }
 }
