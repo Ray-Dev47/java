d7391ac8e43163e1a62b5bac65553ee56670a573
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-04-22 11:24:33 +0100

    task completed

--- index.java
+++ index.java
@@ -1,10 +1,21 @@
-// Write a Java program that takes two numbers as input and display the product of two numbers.
+// Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers. Go to the editor
+// Test Data:
+// Input first number: 125
+// Input second number: 24
 public class index {
     
     public static void main(String args[]) {
-        int a = 50;
-        int b = 30;
-        int result = b * a;
-        System.out.println(result);
+        int a = 125;
+        int b = 24;
+        int result1 = b + a;
+        int result2 = b - a;
+        int result3 = b * a;
+        int result4 = b / a;
+        int result5 = b % a;
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
     }
 }
