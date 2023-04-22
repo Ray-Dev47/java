9ee76a428685544d4da578ae30cd5e7769ec1efa
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-04-22 17:23:37 +0100

    fixed

--- index.java
+++ index.java
@@ -1,8 +1,8 @@
-// Write a Java program to compute the specified expressions and print the output
-// 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))  // test data
-
+// Write a Java program to print the area and perimeter of a circle
+// radius 7.5
 public class index {
     public static void main(String args[]) {
-        System.out.println(4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)));        
+        System.out.println(3.142 * (7.5 * 7.5));    // area     
+        System.out.println(2 * 3.142 * 7.5);    // perimeter     
     }
 }
