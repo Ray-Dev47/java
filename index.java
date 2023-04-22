9f8a8186e0230b2a3bfe1919efa754aac50a06bd
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-04-22 08:58:16 +0100

    challenge completed

---
+++ index.java
@@ -0,0 +1,7 @@
+// Write a Java program to print 'Hello' on screen and then print your name on a separate line
+public class index {
+    public static void main(String args[]) {
+        System.out.println("Hello");
+        System.out.println("My name is Raymond");
+    }
+}
