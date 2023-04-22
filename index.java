1571ed14796e3e7344c68012a37a5308a5ab8ea6
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-04-22 16:41:31 +0100

    completed

--- index.java
+++ index.java
@@ -1,31 +1,12 @@
-// Write a Java program that takes a number as input and prints its multiplication table up to 10
+// Write a Java program to display the following pattern.
 
-// ?Method  one
-// public class index {
-
-//     public static void main(String args[]) {
-//         int num = 8;
-//         for(int i = 1; i <= 10; i++){
-//             System.out.printf( "%d * %d = %d \n", num, i, num * i);  // works 
-//         }
-//     }
-// }
-
-// ***** Method 2
-
-// Using scanner package for custom input
-import java.util.Scanner;
 
 public class index {
     public static void main(String args[]) {
-        try (Scanner num = new Scanner(System.in)) {
-            System.out.println("Enter any number");
-            int ans = num.nextInt();
-            for (int i = 1; i <= 10; i++) {                
-                System.out.println(ans +" x "+i+" = "+ans*i); // works
-            }
-        }
-        ;
+        System.out.println("   J    a   v     v  a ");
+        System.out.println("   J   a a   v   v  a a");
+        System.out.println("J  J  aaaaa   V V  aaaaa");
+        System.out.println(" JJ  a     a   V  a     a");
         
     }
 }
