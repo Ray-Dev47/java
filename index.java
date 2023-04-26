ca47ed567f2dda377a7f6474591b9975c881a999
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-04-26 22:50:06 +0100

    completed

--- index.java
+++ index.java
@@ -1,21 +1,11 @@
-// Write a Java program to swap two variables.
+// Write a Java program to print a face
 public class index {
 
     public static void main(String args[]){
-         int x = 7000 , y = 1000;
-         
-         System.out.println("Before Swap");
-         System.out.println("X is: " + x);  // 7000
-         System.out.println("Y is: " + y);  // 1000
-
-
-         //int temp; //
-         int temp = x;
-         x = y;
-         y = temp;
-
-         System.out.println("After Swap");
-         System.out.println("X is: " + x);  // 1000
-         System.out.println("Y is: " + y);  // 7000
+        System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '-' | ");
+        System.out.println(" +-----+ ");
     }
 }
