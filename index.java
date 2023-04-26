348dfa20dfbf4ff26cfbf9bbe0e9076a9bfcdc7d
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-04-26 16:17:05 +0100

    task completed

--- index.java
+++ index.java
@@ -1,9 +1,20 @@
-//  Write a Java program to print the area and perimeter of a rectangle.
-// Width = 5.5 Height = 8.5
+// Write a Java program to print an American flag on the screen
 public class index {
 
     public static void main(String args[]){
-        System.out.println("Area is " + (5.5 * 8.5));
-        System.out.println("Perimeter is " + 2*(5.5 + 8.5));
+        System.out.println("* * * * * * ================================== ");
+        System.out.println(" * * * * *  ================================== ");
+        System.out.println("* * * * * * ================================== ");
+        System.out.println(" * * * * *  ================================== ");
+        System.out.println("* * * * * * ================================== ");
+        System.out.println(" * * * * *  ================================== ");
+        System.out.println("* * * * * * ================================== ");
+        System.out.println("============================================== ");
+        System.out.println("============================================== ");
+        System.out.println("============================================== ");
+        System.out.println("============================================== ");
+        System.out.println("============================================== ");
+        System.out.println("============================================== ");
+        
     }
 }
