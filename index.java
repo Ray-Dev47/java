4dcbe61f8e990ac52cfc501d0ed8d8b492b96963
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-04-26 22:24:25 +0100

    challenge completed

--- index.java
+++ index.java
@@ -1,20 +1,21 @@
-// Write a Java program to print an American flag on the screen
+// Write a Java program to swap two variables.
 public class index {
 
     public static void main(String args[]){
-        System.out.println("* * * * * * ================================== ");
-        System.out.println(" * * * * *  ================================== ");
-        System.out.println("* * * * * * ================================== ");
-        System.out.println(" * * * * *  ================================== ");
-        System.out.println("* * * * * * ================================== ");
-        System.out.println(" * * * * *  ================================== ");
-        System.out.println("* * * * * * ================================== ");
-        System.out.println("============================================== ");
-        System.out.println("============================================== ");
-        System.out.println("============================================== ");
-        System.out.println("============================================== ");
-        System.out.println("============================================== ");
-        System.out.println("============================================== ");
-        
+         int x = 7000 , y = 1000;
+         
+         System.out.println("Before Swap");
+         System.out.println("X is: " + x);  // 7000
+         System.out.println("Y is: " + y);  // 1000
+
+
+         //int temp; //
+         int temp = x;
+         x = y;
+         y = temp;
+
+         System.out.println("After Swap");
+         System.out.println("X is: " + x);  // 1000
+         System.out.println("Y is: " + y);  // 7000
     }
 }
