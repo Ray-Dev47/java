a691bced48acdeb178ffc7b02579bc3d63759a47
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-04-27 15:37:38 +0100

    task completed

--- index.java
+++ index.java
@@ -1,10 +1,10 @@
-// Write a Java program to add two binary numbers.
+// Write a Java program to multiply two binary numbers.
 
 import java.util.Scanner;
 public class index {
 
     public static void main(String args[]){
-        System.out.println("Welcome to Java Program to add two binary numbers");
+        System.out.println("Welcome to Java Program to multiply two binary numbers");
         Scanner scan = new Scanner(System.in);
 
         System.out.println(" Please Input first Binary Number");
@@ -13,31 +13,31 @@ public class index {
         System.out.println("Please input second Binary Number");
         String second = scan.nextLine();
 
-        String addition = add(first, second);
-        System.out.println("The addition of two binary numbers is " + addition);
+        String multiplication = multiply(first, second);
+        System.out.println("The multiplication of two binary numbers is " + multiplication);
 
-        String  sum = sum(first, second);
-        System.out.println("The sum of two binary numbers is " + sum);
+        String  quotient = quotient(first, second);
+        System.out.println("The quotient of two binary numbers is " + quotient);
 
         scan.close();
         
     }
-    private static String sum(String first, String second) {
+    private static String quotient(String first, String second) {
         return null;
     }
 
     //  next is method to 
     /*
-     * Java method to calculate sum of two binary numbers this method calculate * sum by first converting binary String to binary numbers and then adding * them using binary arithmetic. * * @param first * @param second
+     * Java method to calculate quotient of two binary numbers this method calculate * quotient by first converting binary String to binary numbers and then multiplying * them using binary arithmetic. * * @param first * @param second
      */
 
-     public static String add(String first, String second){
+     public static String multiply(String first, String second){
           int a = Integer.parseInt(first, 2);
           int b = Integer.parseInt(second, 2);
 
-          int sum = a + b;
+          int quotient = a * b;
 
-          return Integer.toBinaryString(sum);
+          return Integer.toBinaryString(quotient);
 
      }
 }
