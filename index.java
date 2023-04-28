b4b038737d9fbba7aee73df0027a3df126189213
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-04-28 12:18:11 +0100

    completed

--- index.java
+++ index.java
@@ -1,41 +1,26 @@
-//  Write a Java program to convert a binary number to a Octal number. 
+//  Write a Java program to convert a octal number to a decimal number.
 
 import java.util.Scanner;
 
 // import java.util.*;
 
+
 public class index {
-    public static void main(String[] args) {
-        int binnum, binnum1, rem, decnum = 0, quot, i = 1, j;
-        int octnum[] = new int[100];
-        Scanner scan = new Scanner(System.in);
-        
-        System.out.print("Input a Binary Number : ");
-        binnum = scan.nextInt();
-        binnum1 = binnum;
-
-        while (binnum > 0) {
-            rem = binnum % 10;
-            decnum = decnum + rem * i;
-            // System.out.println(rem);
-            i = i * 2;
-            binnum = binnum / 10;
-        }
-        scan.close();
-
-        i = 1;
-        quot = decnum;
-
-        while (quot > 0) {
-            octnum[i++] = quot % 8;
-            quot = quot / 8;
-        }
-
-        System.out.print("Equivalent Octal Value of " + binnum1 + " is :");
-        for (j = i - 1; j > 0; j--) {
-            System.out.print(octnum[j]);
-        }
-        System.out.print("\n");
-
-    }
+ 
+public static void main(String[] args) 
+   {
+     Scanner in = new Scanner(System.in);
+     long octal_num, decimal_num = 0;
+     int i = 0;
+     System.out.print("Input any octal number: ");
+    octal_num = in.nextLong();
+    while (octal_num != 0) 
+     {
+      decimal_num = (long)(decimal_num + (octal_num % 10) * Math.pow(8, i++));
+      octal_num = octal_num / 10;
+     }
+     in.close();
+    System.out.print("Equivalent decimal number: " + decimal_num+"\n");
+   }
+   
 }
