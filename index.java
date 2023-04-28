0abc2c1a11771d7138d8520a513e48a1394a7c49
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-04-28 13:23:19 +0100

    task completed

--- index.java
+++ index.java
@@ -1,21 +1,34 @@
-//  Write a Java program to convert a octal number to a hexadecimal number
+//  Write a Java program to convert a hexadecimal to a decimal number.
 
 import java.util.Scanner;
 
 public class index {
-public static void main(String args[])
+   public static int hex_to_decimal(String s)
     {
-        String octal_num, hex_num;
-        int decnum;
-        Scanner in = new Scanner(System.in);
+             String digits = "0123456789ABCDEF";
+             s = s.toUpperCase();
+             int val = 0;
+             for (int i = 0; i < s.length(); i++)
+             {
+                 char c = s.charAt(i);
+                 int d = digits.indexOf(c);
+                 val = 16*val + d;
+             }
+             return val;
+    }
+    public static void main(String args[])
+    {
+        String hexdec_num;
+        int dec_num;
+        Scanner scan = new Scanner(System.in);
 		
-        System.out.print("Input a octal number : ");
-        octal_num = in.nextLine();
+        System.out.print("Input a hexadecimal number: ");
+        hexdec_num = scan.nextLine();
 		
-        decnum = Integer.parseInt(octal_num, 8);
-        hex_num = Integer.toHexString(decnum);
+        dec_num = hex_to_decimal(hexdec_num);
+
+        scan.close();
 		
-        in.close();
-        System.out.print("Equivalent hexadecimal number: "+ hex_num+"\n");
-     }
+        System.out.print("Equivalent decimal number is: " + dec_num+"\n");
+    }
 }
