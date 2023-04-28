17b8abc07c1fbf192512e9387eaaef1b18c4fd9c
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-04-28 14:55:46 +0100

    task completed

--- index.java
+++ index.java
@@ -1,12 +1,11 @@
 //  Write a Java program to convert a hexadecimal to a octal number.
 
 public class index {
-    public static void main(String[] args) {
-        System.out.println("Current JVM version - " + System.getProperty("java.version"));
-        System.out.println("Java Runtime Version: " + System.getProperty("java.runtime.version"));
-        System.out.println("Java Home: " + System.getProperty("java.home"));
-        System.out.println("Java Vendor: " + System.getProperty("java.vendor"));
-        System.out.println("Java Vendor URL: " + System.getProperty("java.vendor.url"));
-        System.out.println("Java Class Path: " + System.getProperty("java.class.path") + "\n");
+    public static void main(String[] args) {    
+        int a = 5;
+        int b = 8;
+        System.out.println(a>b);  // false
+        System.out.println(a!=b);  // true
+        System.out.println(a<b);  // true
     }
 }
