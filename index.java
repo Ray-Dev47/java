3f2282edcd14c8bf60de3bd90f030b4e60449c02
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-04-30 00:36:55 +0100

    task completed

--- index.java
+++ index.java
@@ -1,6 +1,6 @@
-// Write a Java program to compute the area of a hexagon
-// Area of a hexagon = (6 * s^2)/(4*tan(π/6))
-// where s is the length of a side
+//  Write a Java program to compute the area of a polygon.
+// Area of a polygon = (n*s^2)/(4*tan(π/n))
+// where n is n-sided polygon and s is the length of a side
 
 import java.util.Scanner;
 
@@ -9,14 +9,16 @@ public class index{
     public static void main(String args[]){
          Scanner scan = new Scanner(System.in);
          
-         System.out.println("Please enter integer");
-
-         double side = scan.nextDouble();
+         System.out.println("Please enter number of polygon side");
+         double n_side = scan.nextDouble();
+         System.out.println("Please enter length of polygon side");
+         double l_side = scan.nextDouble();
          scan.close();
-         System.out.println("The area of the hexagon is: " + s(side));
+         System.out.println("The area of the hexagon is: " + s(n_side, l_side));
     }
 
-    public static double s(double b){
-         return (6 * Math.pow(b,2))/(4*Math.tan(3.142/6));
+    public static double s(double a, double b){
+        //  return (6 * Math.pow(b,2))/(4*Math.tan(3.142/6));
+         return (a * Math.pow(b,2))/(4*Math.tan(3.142/a));
     }
 }
