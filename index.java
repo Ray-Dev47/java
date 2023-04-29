97105b0c5eb7a41e0680a8ab272ff48eb02fe386
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-04-30 00:04:32 +0100

    task completed

--- index.java
+++ index.java
@@ -1,45 +1,22 @@
-//  Write a Java program and compute the sum of the digits of an integer
+// Write a Java program to compute the area of a hexagon
+// Area of a hexagon = (6 * s^2)/(4*tan(π/6))
+// where s is the length of a side
 
-// import java.util.Scanner;
-
-// public class index {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-//         System.out.println("Please enter first integer");
-//         int first = sc.nextInt();
-//         System.out.println("Please enter second integer");
-//         int second = sc.nextInt();
-//         sc.close();
-
-//         System.out.println("The sum of two integers are: " + sum(first, second));
-//     }
-
-//     public static int sum(int a,int b){
-//         return a + b;
-//     }
-// }
-//  xxxxxxxxxxxxxxxxxxxxxxxxxxx wrong understanding, the idea is to add the two integers separately
-
-
-// Correction - 
 import java.util.Scanner;
 
 public class index{
 
     public static void main(String args[]){
-        int num, input, sum = 0;
          Scanner scan = new Scanner(System.in);
          
          System.out.println("Please enter integer");
-         
-         num = scan.nextInt();
 
-         while(num != 0){
-            input = num % 10;
-            sum += input;
-            num = num / 10; 
-         }
+         double side = scan.nextDouble();
          scan.close();
-         System.out.println("\nSum of Digits = " + sum);
+         System.out.println("The area of the hexagon is: " + s(side));
+    }
+
+    public static double s(double b){
+         return (6 * Math.pow(b,2))/(4*Math.tan(3.142/6));
     }
 }
