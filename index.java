fa37c6426f396d3e4b75dea198b16daa22bd8df3
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-03 00:12:25 +0100

    task completed

--- index.java
+++ index.java
@@ -1,17 +1,48 @@
-//  Write a Java program to print the ascii value of a given character
+//  Write a Java program to input and display your password.
+import java.io.Console;   
 
-import java.util.Scanner;
-public class index {
-  public static void main(String[] args) {
-	System.out.println("Enter any button to reveal the ASCII value: ");  
-    Scanner scan = new Scanner(System.in);
+public class index{
 
-    char input = scan.next().charAt(0);
-    System.out.println("Ascii value of " + input + " is: "  + (int)input);
 
-    scan.close();
-  }
+    public static void main (String args[]) throws Exception {
+        //  logic to check if console is present 
+        Console console = System.console();
+        if(console == null)
+        {
+            System.out.println("Unable to fetch console");
+            return;
+        }
+        // for terminal or shell or bash
+        System.out.print("Enter Username:  ");
+        String userName = console.readLine();
+
+        System.out.print("Enter Password:  ");
+        char ch[] = console.readPassword();
+        String password = String.valueOf(ch);
+
+
+        System.out.println("Username entered is: " + userName);
+        System.out.println("Password entered is: " + password);
+    }
 }
 
-
-
+// method 2 
+// import java.io.Console;
+// public class index {
+// 	public static void main(String[] args) {
+// 		Console cons;
+// 		if ((cons = System.console()) != null) {
+// 			char[] pass_ward = null;
+// 			try {
+// 				pass_ward = cons.readPassword("Input your Password:");
+// 				System.out.println("Your password was: " + new String(pass_ward));
+// 			} finally {
+// 				if (pass_ward != null) {
+// 					java.util.Arrays.fill(pass_ward, ' ');
+// 				}
+// 			}
+// 		} else {
+// 			throw new RuntimeException("Can't get password...No console");
+// 		}
+// 	}
+// }
