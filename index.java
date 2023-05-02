2237e0851ecf7fc370b8ffb354c0566596c3c03e
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-02 13:21:19 +0100

    task completed

--- index.java
+++ index.java
@@ -1,5 +1,12 @@
-// Write a Java program to reverse a string on input
-
+// Write a Java program to count the letters, spaces, numbers and other characters of an input string
+/*
+ * Expected Output
+The string is :  Aa kiu, I swd skieo 236587. GH kiu: sieo?? 25.33
+letter: 23                                               
+space: 9                                                 
+number: 10                                               
+other: 6
+ */
 
 import java.util.Scanner;
 
@@ -9,17 +16,43 @@ public class index{
          Scanner scan = new Scanner(System.in);
           
          System.out.println("Enter any string"); 
-         String str = scan.nextLine();
-         char[] ch = str.toCharArray();
-         
-         scan.close();
-         System.out.println("Reversed String is: ");
+         String strInput = scan.nextLine();
+         countMethod(strInput);
 
-         int j = ch.length;
-         for(int i = j; i > 0; i--){
-            System.out.print(ch[i-1]);
-         }
+         scan.close();
     }
+
+    private static void countMethod(String str) {
+        char[] ch = str.toCharArray();
+        int letter = 0;
+        int space = 0;
+        int num = 0;
+        int otherChars = 0;
+
+        for(int i = 0; i < str.length(); i++){
+            if( Character.isLetter(ch[i])){
+                letter++;
+            }
+            else if(Character.isDigit(ch[i])){
+                num++;
+            }
+            else if(Character.isSpaceChar(ch[i])){
+                space++;
+            }
+            else{
+                otherChars++;
+            }
+        }
+        System.out.println("Entered str: " + str);
+        System.out.println("Letter: " + letter);
+        System.out.println("number: " + num);
+        System.out.println("Space: " + space);
+        System.out.println("Other: " + otherChars);
+
+    }
+    
 
         
 }
+
+
