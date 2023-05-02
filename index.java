8408cec274563a52065ff7ed840cecc2c814a506
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-02 14:42:28 +0100

    task completed

--- index.java
+++ index.java
@@ -1,54 +1,35 @@
-// Write a Java program to count the letters, spaces, numbers and other characters of an input string
+// Write a Java program to create and display unique three-digit number using 1, 2, 3, 4. Also count how many three-digit numbers are there
 /*
  * Expected Output
-The string is :  Aa kiu, I swd skieo 236587. GH kiu: sieo?? 25.33
-letter: 23                                               
-space: 9                                                 
-number: 10                                               
-other: 6
+TExpected Output
+
+123                                                      
+124                                                      
+...                                            
+                                                   
+431                                                      
+432                                                      
+Total number of the three-digit-number is 24
  */
 
-import java.util.Scanner;
 
 public class index{
 
     public static void main(String args[]){
-         Scanner scan = new Scanner(System.in);
-          
-         System.out.println("Enter any string"); 
-         String strInput = scan.nextLine();
-         countMethod(strInput);
-
-         scan.close();
-    }
-
-    private static void countMethod(String str) {
-        char[] ch = str.toCharArray();
-        int letter = 0;
-        int space = 0;
-        int num = 0;
-        int otherChars = 0;
-
-        for(int i = 0; i < str.length(); i++){
-            if( Character.isLetter(ch[i])){
-                letter++;
-            }
-            else if(Character.isDigit(ch[i])){
-                num++;
-            }
-            else if(Character.isSpaceChar(ch[i])){
-                space++;
-            }
-            else{
-                otherChars++;
+        int total = 0;
+        for (int i = 1; i < 4; i++){
+            for(int j = 1; j < 4; j++){
+                for(int k = 1; k < 4; k++){
+                    if(k != i && k !=  j && i != j){
+                        total++;
+                        System.out.println(i + "" + j + "" + k );
+                    }
+                }
             }
         }
-        System.out.println("Entered str: " + str);
-        System.out.println("Letter: " + letter);
-        System.out.println("number: " + num);
-        System.out.println("Space: " + space);
-        System.out.println("Other: " + otherChars);
-    }     
+        System.out.println("Total number of the three-digit-number is " + total);
+         
+    }
 }
 
 
