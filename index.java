c99c55bf1ef93a07651c8cf4447d62b354421685
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-04 14:15:30 +0100

    completed

--- index.java
+++ index.java
@@ -1,15 +1,21 @@
-//   Write a Java program to print odd numbers from 1 to 99. Prints one number per line
+//  Write a Java program to accept a number and check whether the number is even or not. Prints 1 if the number is even or 0 if odd
 /*
  */
+import java.util.Scanner;
+
 public class index {
 
 	public static void main(String args[]) {
-		int num = 99;
-        for(int i = 0; i <= num; i++){
-            if(i % 2 != 0){
-                System.out.println(i);
+        Scanner sc = new Scanner(System.in);
+    System.out.println("Please enter integer to check if odd or even");
+		int num = sc.nextInt();
+        
+            if(num % 2 == 0){
+                System.out.println("1....Even");
+            } else if(num % 2 != 0){
+                System.out.println("0....Odd");
             }
-        }
+        sc.close();
 	}
 }
 
