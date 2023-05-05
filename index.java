4a83906fd7a3c73f501ccca1b9498ae5f20c9f4e
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-05 22:58:59 +0100

    completed

--- index.java
+++ index.java
@@ -1,7 +1,9 @@
-//  Write a Java program to find the number of values in a given range divisible by a given value.
+//  Write a Java program to accept an integer and count the factors of the number. 
 /*
-For example x = 5, y=20 and p =3, find the number of integers within the range x..y and that are divisible by p i.e. { i :x ≤ i ≤ y, i mod p = 0 }
-Sample Output: 5
+Sample Output:
+
+Input an integer: 25                                                   
+3
  */
 
 import java.util.Scanner;
@@ -9,24 +11,30 @@ public class index {
 
 	public static void main(String args[])   {
         Scanner sc = new Scanner(System.in);
-             System.out.println("Input first value");
+             System.out.println("Input Integer");
             int x = sc.nextInt();
-            System.out.println("Input second value");
-            int y = sc.nextInt();
-            System.out.println("Input third value");
-            int z = sc.nextInt();
+            
         sc.close();
-        System.out.println("Ans: " + ans(x, y, z));
-        
-
+        System.out.println("Ans: " + result(x));
     }
 
-          static int ans(int a, int b, int c)
-          {
+        // create function to find the factors of given number.
+	public static int result(int num) 
+	{	
+		int ctr = 0;
+		for(int i=1; i<=(int)Math.sqrt(num); i++)
+	    {
+	        if(num%i==0 && i*i!=num)
+	        {
+	            ctr+=2;
+	        } 
+	        else if (i*i==num) 
+	        {
+	            ctr++;
+	        }
+	    }
+	        return ctr;
           
-          if (a % c == 0)
-               return (b / c) - (a / c) + 1;
-          return (b / c) - (a / c);
           }
       }
 
