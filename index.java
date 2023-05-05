828b8adacb71f0fd0ab909b1f62b4ff9b82f6885
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-05 10:07:43 +0100

    task completed

--- index.java
+++ index.java
@@ -1,27 +1,53 @@
-//  Write a Java program to convert a string to an integer.
+//   Write a Java program to calculate the sum of two integers and return true if the sum is equal to a third integer.
 /*
  */
-import java.util.*;
-// import java.util.Scanner;
+
+import java.util.Scanner;
 public class index {
 
 	public static void main(String args[])   {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Input a number(string): ");
-        String str1 = in.nextLine();
-		int result;
-        try {
-            result = Integer.parseInt(str1);
-        } catch (NumberFormatException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first Integer");
+        int int1 = sc.nextInt();
+        System.out.println("Enter the second integer");
+        int int2 = sc.nextInt();
+        System.out.println("Enter the third integer");
+        int int3 = sc.nextInt();
+
+        sc.close();
+        if(int1 + int2 == int3 ){
+             System.out.println("The result is: True");
+        } else{
+            System.out.println("The result is: False");
         }
-        in.close();
-        System.out.printf("The integer value is: %d",result);
-		System.out.printf("\n");
+        
     }
         
 }
+
+
+
+// Method 2 -
+// import java.util.*;
+//  public class Exercise52 {
+//  public static void main(String[] args)
+//     {
+//         Scanner in = new Scanner(System.in);
+//         System.out.print("Input the first number : ");
+//         int x = in.nextInt();  
+// 		System.out.print("Input the second number: ");
+// 		int y = in.nextInt(); 
+// 		System.out.print("Input the third number : ");
+//         int z = in.nextInt(); 
+//         System.out.print("The result is: "+sumoftwo(x, y, z));
+// 		System.out.print("\n");
+//     }
+    
+//     public static boolean sumoftwo(int p, int q, int r)
+//       {	
+//          return ((p + q) == r || (q + r) == p || (r + p) == q);	
+// 	   }
+// }
 
 
 
