1469442260393622cf1bee77e8879be4c99ffe62
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-05 22:14:53 +0100

    completed

--- index.java
+++ index.java
@@ -1,5 +1,7 @@
-//   Write a Java program that accepts three integers from the user. It returns true if the second number is higher than the first number and the third number is larger than the second number. If "abc" is true, the second number does not need to be larger than the first number.
+//  Write a Java program to find the number of values in a given range divisible by a given value.
 /*
+For example x = 5, y=20 and p =3, find the number of integers within the range x..y and that are divisible by p i.e. { i :x ≤ i ≤ y, i mod p = 0 }
+Sample Output: 5
  */
 
 import java.util.Scanner;
@@ -7,15 +9,26 @@ public class index {
 
 	public static void main(String args[])   {
         Scanner sc = new Scanner(System.in);
-             System.out.println("Input an integer to get the time");
-             int time = sc.nextInt();
-             int hours = time / 3600;
-             int minutes = (time % 3600) / 60;
-             int seconds = time % 60;
+             System.out.println("Input first value");
+            int x = sc.nextInt();
+            System.out.println("Input second value");
+            int y = sc.nextInt();
+            System.out.println("Input third value");
+            int z = sc.nextInt();
         sc.close();
-         System.out.println(hours + ":" + minutes + ":" + seconds );
+        System.out.println("Ans: " + ans(x, y, z));
+        
+
     }
-}
+
+          static int ans(int a, int b, int c)
+          {
+          
+          if (a % c == 0)
+               return (b / c) - (a / c) + 1;
+          return (b / c) - (a / c);
+          }
+      }
 
 
 
