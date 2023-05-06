ce69443e14ed5474af407785df7fe9f16f0a0270
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-06 20:52:00 +0100

    task complted

--- index.java
+++ index.java
@@ -1,22 +1,28 @@
-//Write a Java program to reverse a word.
 /*
+Write a Java program that accepts three integer values and returns true if one is 20 or more less than the others' subtractions. Go to the editor
+Sample Output:
+
+Input the first number : 15                                            
+Input the second number: 20                                            
+Input the third number : 25                                            
+false
  */
 
 
 // import java.util.*;
-import java.util.Scanner;
+import java.util.*;
  public class index {
-     public static void main(String[] args){
-	 Scanner in = new Scanner(System.in);
-     System.out.print("Input a word: ");
-	 String line = in.nextLine();
-      char [] ch = line.toCharArray();
-	 
-      int j = ch.length;
-         for(int i = j; i > 0; i--){
-            System.out.print(ch[i-1]);
-         }
-      in.close();
+ public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input the first number : ");
+        int x = in.nextInt();  
+		System.out.print("Input the second number: ");
+		int y = in.nextInt(); 
+		System.out.print("Input the third number : ");
+        int z = in.nextInt(); 
+        in.close();
+        System.out.println((Math.abs(x - y) >= 20 || Math.abs(y - z) >= 20 || Math.abs(z - x) >= 20));
     }
 }
 
