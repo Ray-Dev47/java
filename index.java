2c32e521c33b804103b35021ce2efc363dce90d5
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-09 16:01:15 +0100

    completed

--- index.java
+++ index.java
@@ -3,20 +3,34 @@ Write a Java program to calculate the modules of two numbers without using any i
  */
 
 // import java.util.*;
-import java.util.*;
 
 public class index {
      public static void main(String[] args) {
-          Scanner in = new Scanner(System.in);
-        System.out.print("Input the first number : ");
-        int a = in.nextInt();  
-		System.out.print("Input the second number: ");
-		int b = in.nextInt(); 
-		int divided = a / b;
-		int result = a - (divided * b);
-          in.close();
-          
-		System.out.println(result); 
+          // Scanner in = new Scanner(System.in);
+          int sum = 1;
+          int ctr = 0; 
+          int num = 0; 
+      
+             while (ctr < 100) {
+                 num++; 
+                 if (num % 2 != 0) { 
+                 // check if the number is even
+                     if (is_Prime(num)) {
+                         sum += num; 
+                         ctr++; 
+                     }
+                 }	
+             }
+             System.out.println("\nSum of the first 100 prime numbers is: "+sum); 	
+          }
+      
+             public static boolean is_Prime(int num) {
+             for (int i = 3; i * i <= num; i+= 2) {
+                 if (num % i == 0) {
+                     return false; 
+                 }
+             }
+             return true;
      }
 
     
