6ea0d7d01d7138dc5eb0590cbcc32f0951fc9d44
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-10 00:32:12 +0100

    task completed

--- index.java
+++ index.java
@@ -1,37 +1,24 @@
 /*
-Write a Java program to calculate the modules of two numbers without using any inbuilt modulus operator.
- */
+Write a Java program to insert a word in the middle of another string. Go to the editor
+Insert "Tutorial" in the middle of "Python 3.0", so the result will be Python Tutorial 3.0.
+Sample Output:
 
-// import java.util.*;
+Python Tutorial 3.0
+ */
 
 public class index {
      public static void main(String[] args) {
-          // Scanner in = new Scanner(System.in);
-          int sum = 1;
-          int ctr = 0; 
-          int num = 0; 
-      
-             while (ctr < 100) {
-                 num++; 
-                 if (num % 2 != 0) { 
-                 // check if the number is even
-                     if (is_Prime(num)) {
-                         sum += num; 
-                         ctr++; 
-                     }
-                 }	
-             }
-             System.out.println("\nSum of the first 100 prime numbers is: "+sum); 	
-          }
-      
-             public static boolean is_Prime(int num) {
-             for (int i = 3; i * i <= num; i+= 2) {
-                 if (num % i == 0) {
-                     return false; 
-                 }
-             }
-             return true;
+         String stringOne = "Raymond loves apple";
+         String stringTwo = "Red ";
+         int index = 13;
+         
+         String result = new String();
+         for(int i = 0; i < stringOne.length(); i++){
+               result += stringOne.charAt(i);
+               if(i == index){
+                    result += stringTwo;
+               }
+         }
+         System.out.println("The result is: " + result);
      }
-
-    
 }
