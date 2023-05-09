858cf44824326052f39ec59add6f9e981ed5673b
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-09 15:33:44 +0100

    completed

--- index.java
+++ index.java
@@ -1,10 +1,5 @@
 /*
-program that accepts two integer values between 25 and 75 and returns true if there is a common digit in both numbers
-Sample Output:
-
-Input the first number : 12                                            
-Input the second number: 13   
-true
+Write a Java program to calculate the modules of two numbers without using any inbuilt modulus operator.
  */
 
 // import java.util.*;
@@ -13,22 +8,14 @@ import java.util.*;
 public class index {
      public static void main(String[] args) {
           Scanner in = new Scanner(System.in);
-          System.out.print("Input the 1st number : ");
-          int a = in.nextInt();
-          System.out.print("Input the 2nd number: ");
-          int b = in.nextInt();
-          in.close();
-          System.out.println("Result: " + result(a, b));
+        System.out.print("Input the first number : ");
+        int a = in.nextInt();  
+		System.out.print("Input the second number: ");
+		int b = in.nextInt(); 
+		int divided = a / b;
+		int result = a - (divided * b);
+		System.out.println(result); 
      }
 
-     public static boolean result(int p, int q) {
-         
-          if (p<25 || q>75)
-		   return false;
-	   int x = p % 10;
-	   int y = q % 10;
-	   p /= 10;
-	   q /= 10;
-	   return (p == q || p == y || x == q || x == y);
-     }
+    
 }
