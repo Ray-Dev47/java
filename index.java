b3bdf789c4cffb5021b11faa68fea93b4c6de901
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-11 00:41:17 +0100

    task well completed

--- index.java
+++ index.java
@@ -1,22 +1,19 @@
 /*
-Write a Java program to create a string in the form of short_string + long_string + short_string from two strings. The strings must not have the same length. Go to the editor
+Write a Java program to create the concatenation of the two strings except removing the first character of each string. The length of the strings must be 1 and above. Go to the editor
 Test Data: Str1 = Python
 Str2 = Tutorial
-Sample Output:
-
-PythonTutorialPython
+Sample Output: ythonutorial
  */
-//import java.lang.*;
+// import java.lang.*;
+import java.util.Scanner;
 public class index {
      public static void main(String[] args) {
-          String str1 = "Python";    
-          String str2 = "Tutorial"; 
-          
-          if(str1.length() >= str2.length())
-               System.out.println( str2+str1+str2);
-          else
-          System.out.println(str1+str2+str1);	
-          
-
+          Scanner sc = new Scanner(System.in);
+          System.out.println("Please enter first String");
+          String str1 = sc.nextLine();  
+          System.out.println("Please enter second String");  
+          String str2 = sc.nextLine(); 
+          sc.close();
+               System.out.println(str1.substring(1) + str2.substring(1));
      }
 }
