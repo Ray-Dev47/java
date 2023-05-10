5e6280cc452d30e31c86b554db3365dd9dca2388
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-10 01:29:25 +0100

    completed

--- index.java
+++ index.java
@@ -1,17 +1,15 @@
 /*
-Write a Java program to create another string of 4 copies of the last 3 characters of the original string. The original string length must be 3 and above. Go to the editor
+Write a Java program to extract the first half of a even string. Go to the editor
+Test Data: Python
 Sample Output:
-
-3.03.03.03.0 
+Pyt
  */
 //import java.lang.*;
- public class index{
- public static void main(String[] args)
- {
-   String main_string = "Python 3.0";
-      
-   String last_three_chars = main_string.substring(main_string.length() - 3);
-   System.out.println(last_three_chars + last_three_chars + last_three_chars + last_three_chars);
-  }
-    }
+public class index {
+     public static void main(String[] args) {
+          String str = "Python";
+               String result = str.substring(0, 3);
+          System.out.println(result);
 
+     }
+}
