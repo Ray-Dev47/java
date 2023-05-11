106c2d63c87c64e402b8d700455e2721f7fe531c
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-11 01:56:59 +0100

    task completed

--- index.java
+++ index.java
@@ -1,19 +1,27 @@
+
 /*
-Write a Java program to create the concatenation of the two strings except removing the first character of each string. The length of the strings must be 1 and above. Go to the editor
-Test Data: Str1 = Python
-Str2 = Tutorial
-Sample Output: ythonutorial
+Write a Java program to create a string taking the first three characters from a given string. If the string length is less than 3 use "#" as substitute characters. Go to the editor
+Test Data: Str1 = " "
+Sample Output:
+
+###
  */
 // import java.lang.*;
 import java.util.Scanner;
+
 public class index {
      public static void main(String[] args) {
           Scanner sc = new Scanner(System.in);
           System.out.println("Please enter first String");
-          String str1 = sc.nextLine();  
-          System.out.println("Please enter second String");  
-          String str2 = sc.nextLine(); 
+          String str1 = sc.nextLine();
+          if (str1.length() >= 3) {
+               System.out.println(str1.substring(0, 3));
+
+          } else if (str1.length() == 1)
+               System.out.println((str1.charAt(0) + "##"));
+          else
+               System.out.println("###");
+
           sc.close();
-               System.out.println(str1.substring(1) + str2.substring(1));
      }
 }
