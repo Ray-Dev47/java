7c013e587aafeb429df9e424189b8bf099bbf039
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-12 02:45:42 +0100

    task 77 completed

--- index.java
+++ index.java
@@ -1,23 +1,32 @@
 
 /*
-Write a Java program to test if the first and last element of two integer arrays are the same. The array length must be greater than or equal to 2. Go to the editor
-Test Data: array1 = 50, -20, 0, 30, 40, 60, 12
-array2 = 45, 20, 10, 20, 30, 50, 11
+Write a Java program to create an array of length 2 from two integer arrays with three elements. The newly created array will contain the first and last elements from the two arrays. Go to the editor
+Test Data: array1 = 50, -20, 0
+array2 = 5, -50, 10
 Sample Output:
 
-false
+Array1: [50, -20, 0]                                                   
+Array2: [5, -50, 10]                                                   
+New Array: [50, 10]
  */
 // import java.lang.*;
+
+import java.util.Arrays;
+
 public class index {
      public static void main(String[] args) {
-          int[] arr1 = { 50, -20, 0, 30, 40, 60, 70 };
-          int[] arr2 = { 40, 20, 10, 20, 30, 50, 45 };
+          int[] arr1 = { 50, -20, 0 };
+          int[] arr2 = { 5, -50, 10 };
           int firstArr1 = arr1[0];
-          int lastArr1 = arr1[arr1.length - 1];
-          int firstArr2 = arr2[0];
           int lastArr2 = arr2[arr2.length - 1];
+          
+          int [] newArray = new int[2]; 
+          newArray[0] = firstArr1;
+          newArray[1] = lastArr2;
 
-          System.out.println(firstArr1 == lastArr1 &&  firstArr2 == lastArr2);
+          System.out.println("Array1: " + Arrays.toString(arr1) );
+          System.out.println("Array2: " + Arrays.toString(arr2) );
+          System.out.println("New Array: " + Arrays.toString(newArray) );
           
      }
 }
