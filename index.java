c457f2389327854007366557e94745e9e9ea5829
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-13 22:41:44 +0100

    task 81 completed

--- index.java
+++ index.java
@@ -1,24 +1,23 @@
 
 /*
-Write a Java program to get the largest value between the first and last elements of an array (length 3) of integers. Go to the editor
+Write a Java program to swap the first and last elements of an array (length must be at least 1) and create another array. Go to the editor
 Sample Output:
 
 Original Array: [20, 30, 40]                                           
-Larger value between first and last element: 40 
+New array after swaping the first and last elements: [40, 30, 20] 
  */
 
- class index {    
+import java.util.Arrays;
+
+class index {    
      public static void main(String[] args) {    
             //Initialize array     
-            int [] arr =  {20, 30, 40};     
-            
-            int max = arr[0];
+            int [] arr =  {20, 30, 40};
+            System.out.println("The original array is: " + Arrays.toString(arr)); 
 
-            for(int i = 0; i < arr.length; i++){
-               if(arr[i] > max){
-                    max = arr[i];
-               }
-            }
-            System.out.println("Max element in the array is: " + max);
+            int arrTemp = arr[0];
+            arr[0] = arr[arr.length - 1];
+            arr[arr.length - 1] = arrTemp;
+            System.out.println("New array after swaping the first and last elements: " + Arrays.toString(arr));
         }    
     }    
