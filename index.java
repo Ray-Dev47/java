79e87bf4ade442a4787845b9c905ca912ce2a755
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-15 11:16:49 +0100

    task 83 completed

--- index.java
+++ index.java
@@ -5,7 +5,7 @@ Sample Output:
 
 Array1: [1, 3, -5, 4]                                                                                                                    
 Array2: [1, 4, -5, -2]                                                                                                                       
-Result: 1 12 25 -8
+Result: 1 12 25 -8 // ans 
  */
 
 // import java.util.Arrays;
