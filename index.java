c7b80de2219c419a7d62db05c44b0669cfe7f4fb
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-17 16:33:53 +0100

    task 90 completed

--- index.java
+++ index.java
@@ -1,6 +1,6 @@
 
 /*
-Write a Java program to check whether a security manager has already been established for the current application or not
+Write a Java program to get the value of environment variables PATH, TEMP, USERNAME.
 */
 
 // import java.io.*;
@@ -8,9 +8,14 @@ Write a Java program to check whether a security manager has already been establ
 public class index {
 	public static void main(String[] args) {
 		
+          System.out.println("Get envronment variables path: ");
+          System.out.println(System.getenv("PATH"));  //
 
-          System.out.println("Get sys security manager");
-          System.out.println(System.getSecurityManager());  // deprecated
+          System.out.println("Get envronment variables Temp: ");
+          System.out.println(System.getenv("TEMP"));  //
+
+          System.out.println("Get envronment variables Username: ");
+          System.out.println(System.getenv("USERNAME"));  //
 
     }
 }
