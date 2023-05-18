6f752e666253cc897ab7c282177a79f69ef42ba0
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-18 12:38:03 +0100

    task 91 completed successfully

--- index.java
+++ index.java
@@ -1,22 +1,25 @@
 
 /*
-Write a Java program to get the value of environment variables PATH, TEMP, USERNAME.
+Write a Java program to measure how long code executes in nanoseconds.
 */
 
 // import java.io.*;
 
 public class index {
 	public static void main(String[] args) {
-		
-          System.out.println("Get envronment variables path: ");
-          System.out.println(System.getenv("PATH"));  //
+		 long startTime = System.nanoTime();
+      
+        // action to display first 10 natural numbers and then we will measure the time it takes to execute this 
+        // int i;
+        System.out.println("The first 10 ntural numbers are:\n:");
+        for(int i = 1; i < 10; i++){
+            System.out.println(i);
+        }
+         
+        long endTime = System.nanoTime();
+        long totalTime = endTime - startTime;
 
-          System.out.println("Get envronment variables Temp: ");
-          System.out.println(System.getenv("TEMP"));  //
-
-          System.out.println("Get envronment variables Username: ");
-          System.out.println(System.getenv("USERNAME"));  //
-
+        System.out.println("Total time it takes to execute in nanoSeconds is: " + totalTime);
     }
 }
  
