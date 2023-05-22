cdb5f9b3f2df9b3426d27e74bf9bcb9f8f724184
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-22 12:49:14 +0100

    task 116 completed

--- index.java
+++ index.java
@@ -1,50 +1,21 @@
 
 /*
-Write a Java program to check if a positive number is a palindrome or not.
-Input a positive integer: 151
-Is 151 is a palindrome number?
-true
+Write a Java program that iterates integers from 1 to 100. For multiples of three print "Fizz" instead of the number and print "Buzz" for five. When the number is divided by three and five, print "fizz buzz"
 */
+    public class index {
+    
+        public static void main(String args[]) {
 
-import java.util.Scanner;
-
-public class index {
-
-    public static void main(String args[]){
-       
-        System.out.println("Please Enter a number : ");
-        Scanner sc = new Scanner(System.in);
-        int palindrome = sc.nextInt();
-        sc.close();
-       
-        if(isPalindrome(palindrome)){
-            System.out.println("Number : " + palindrome
-                   + " is a palindrome");
-        }else{
-            System.out.println("Number : " + palindrome
-                   + " is not a palindrome");
-        }      
-       
-    }
- 
-    /*
-     * Java method to check if a number is palindrome or not
-     */
-    public static boolean isPalindrome(int number) {
-        int palindrome = number; // copied number into variable
-        int reverse = 0;
-
-        while (palindrome != 0) {
-            int remainder = palindrome % 10;
-            reverse = reverse * 10 + remainder;
-            palindrome = palindrome / 10;
+                for (int i = 0; i <= 100; i++) {
+                    if(i % 3 == 0){
+                        System.out.println(i +  " Fizz divisible by 3");
+                    } else if(i % 5 == 0){
+                        System.out.println( i + " Buzz divisible by 5");
+                    } else if (i % 3 == 0 &&  i % 5 == 0 ){
+                        System.out.println(i + " Fizz buxx divisible by 3 and 5");
+                    }else{
+                        System.out.println(i + " Other are not divisible");
+                    }
+                }
         }
-
-        // if original and the reverse of number is equal means
-        // number is palindrome in Java
-        if (number == reverse) {
-            return true;
-        }
-        return false;
     }
-}
