1910982dc9b76a5644a46990bd0b41039107dda3
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-22 15:25:31 +0100

    task 118 completed

--- index.java
+++ index.java
@@ -1,26 +1,50 @@
 
 /*
-Write a Java program to compute the square root of a given number.
-Input a positive integer: 25
-Square root of 25 is: 5
+Write a Java program to get the first occurrence (Position starts from 0.) of a string within a given string.
 */
 
-import java.util.Scanner;
-import java.lang.Math;
     public class index {
     
         public static void main(String args[]) {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("input a positive integer");
-            double nums = sc.nextDouble();
-
-
-            sc.close();
-            System.out.println("Square root of " + nums + " is " + sqrtCalc(nums));
-
-                         
+            String name = "pythonpy";
+            System.out.println(name.indexOf("py"));         
         }
-        public static double sqrtCalc(double a) {
-            return Math.sqrt(a);
-        }  
     }
+
+
+// correction
+// import java.util.Scanner;
+// public class Main {
+// public static void main(String[] args) {
+//    		String s = "Python";
+//    		//String t = "Py";
+//    		  String t = "yt";
+//    		// String t = "ab";   		
+//    System.out.printf(String.valueOf(strStr(s, t)));
+// 		  }     
+// public static int strStr(String source, String target) {
+//         if (source == null || target == null) {
+//             return -1;
+//         }
+//         if ("".equals(target) || source.equals(target)) {
+//             return 0;
+//         }
+//         int i = 0;
+//         int last = source.length() - target.length() + 1;
+//         while (i < last) {
+//             if (source.charAt(i) == target.charAt(0)) {
+//                 boolean equal = true;
+//                 for (int j = 0; j < target.length() && equal; ++j) {
+//                     if (source.charAt(i + j) != target.charAt(j)) {
+//                         equal = false;
+//                     }
+//                 }
+//                 if (equal) {
+//                     return i;
+//                 }
+//             }
+//             ++i;
+//         }
+//         return -1;
+//     }
+// }
