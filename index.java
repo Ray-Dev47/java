3b1502a4c7fe1bbd7fd9b3aed13408a92cd027e0
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-23 13:23:13 +0100

    task 120 completed

--- index.java
+++ index.java
@@ -1,29 +1,52 @@
 
 /*
-Write a Java program to get the first occurrence (Position starts from 0.) of an element of a given array.
+Write a Java program that searches for a value in an m x n matrix.
 */
 
 
 
-// import java.util.Scanner;
 public class index {
-public static void main(String[] args) {
-    int nums[] = {2, 4, 6, 7, 8};
-    int target = 7;
-        int lower = 0;
-        int upper = nums.length - 1;
-        int index = -1;
-        while (lower <= upper) {
-            int mid = (lower + upper) >> 1;
-            if (nums[mid] == target) {
-                index = mid;
+    public static void main(String[] args) {
+     // int target = 5;
+      int target = 0;
+      int[][] matrix = new int[3][3];
+            for (int row = 0; row < 3; row ++)
+                for (int col = 0; col < 3; col++)
+                    matrix[row][col] = (1 + row * 3 + col);
+    
+            for (int row = 0; row < 3; row ++)
+            {
+                for (int col = 0; col < 3; col++)
+                  {
+                    System.out.print(matrix[row][col]+" ");
+                    if (col == 2)
+                    System.out.println();
+                  }
             }
-            if (nums[mid] >= target) {
-                upper = mid - 1;
-            } else {
-                lower = mid + 1;
-            }
+          System.out.print(Boolean.toString(searchMatrix(matrix, target))); 
         }
-        System.out.print("Position of "+target +" is "+index);
+      public static boolean searchMatrix(int[][] matrix, int target) {
+            if (matrix.length == 0 || matrix[0].length == 0) {
+                return false;
+            }
+            int m = matrix.length;
+            int n = matrix[0].length;
+            int lower = 0;
+            int higher = m * n - 1;
+            while (lower <= higher) {
+                int mid = (lower + higher) >> 1;
+                int val = matrix[mid / n][mid % n];
+                if (val == target) {
+                    return true;
+                }
+                if (val < target) {
+                    lower = mid + 1;
+                } else {
+                    higher = mid - 1;
+                }
+            }
+            return false;
+        }
     }
-}
+    
+    
