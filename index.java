1584c34cd46ca2107d5aed0496bd5e17e9c8d54b
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-30 14:04:00 +0100

    task 143 completed

--- index.java
+++ index.java
@@ -5,26 +5,37 @@ String has all unique characters: false
 */
 
 // import java.util.Arrays;
-public class index {
-    boolean uniqueChar(String str){
-        for (int i = 0; i < str.length(); i++) {
-            for (int j = i + 1; j < str.length(); j++) {
-                 if(str.charAt(i) == str.charAt(j)){
-                    return false;
-                 }
+public  class index {
+    /**
+     * @param s: The first string
+     * @param b: The second string
+     * @return true or false
+     */
+   public static boolean anagram_test(String str1, String str2) {
+        if (str1 == null || str2 == null) {
+            return false;
+        } else if (str1.length() != str2.length()) {
+            return false;
+        } else if (str1.length() == 0 && str2.length() == 0) {
+            return true;
+        }
+        int[] count = new int[256];
+        for (int i = 0; i < str1.length(); i++) {
+            count[str1.charAt(i)]++;
+            count[str2.charAt(i)]--;
+        }
+        for (int num : count) {
+            if (num != 0) {
+                return false;
             }
-            
         }
         return true;
     }
     public static void main(String[] args) {
-       index obj = new index();
-       String words = "aeioue";
-
-       if(obj.uniqueChar(words)){
-           System.out.println("String: " + words + " has unique characters");
-       }else{
-        System.out.println("String: " + words + " has duplicate characters");
-       }
-   }
+		String str1 = "wxyz";
+		String str2 = "zyxw";
+		System.out.println("String-1 : "+str1);
+		System.out.println("String-2 : "+str2);
+		System.out.println("Check if two given strings are anagrams or not?: "+anagram_test(str1,str2));
+	}		
 }
