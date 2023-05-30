9800acca60a6fb98c494966ed442fc41e5eb09b2
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-30 22:53:32 +0100

    final task completed

--- index.java
+++ index.java
@@ -1,77 +1,57 @@
 /*
-Write a Java program to remove the nth element from the end of a given list.
+Write a Java program to test if a binary tree is a subtree of another binary tree.
 Sample Output:
-Original node:
-1 2 3 4 5
-After removing 2nd element from end:
-1 2 3 5
+Original strings: xxyz yxzx
+true
 */
 
-// import java.util.Arrays;
-// Java program to remove an element
-// from a specific index from an array
+public  class  index {
+    /**
+   * @param T1, T2: The roots of binary tree.
+   * @return: True if T2 is a subtree of T1, or false.
+   */
+      public static boolean is_Subtree(TreeNode T1, TreeNode T2) {
+      if (T2 == null) return true;
+      if (T1 == null) return false;
+      if (is_Same(T1, T2)) return true;
+      if (is_Subtree(T1.left, T2) || is_Subtree(T1.right, T2)) return true;
+      return false;
+  }
 
-import java.util.Arrays;
+  public static boolean is_Same(TreeNode t1, TreeNode t2) {
+      if (t1 == null || t2 == null) {
+          return t1 == t2;
+      } else if (t1.val != t2.val) {
+          return false;
+      } else {
+          return is_Same(t1.left, t2.left) && is_Same(t1.right, t2.right);
+      }
+  }
+ /* Driver program to test above functions */
+  public static void main(String[] args) {
+      TreeNode t1 = new TreeNode(1);
+      TreeNode t2 = new TreeNode(2);
+      TreeNode t3 = new TreeNode(3);
+    //   TreeNode t4 = new TreeNode(4);
+      t1.left  = t2;
+      t1.right = t3;
+     
+      TreeNode n1 = new TreeNode(1);
+      TreeNode n2 = new TreeNode(2);
+      TreeNode n3 = new TreeNode(3);
+    //   TreeNode n4 = new TreeNode(4);
+      n1.left  = n2;
+      n1.right = n3;
+    
+      System.out.println(is_Subtree(t1, n1));
+  }
+}
+class TreeNode {
+  public int val;
+  public TreeNode left, right;
 
-class index {
-
-	// Function to remove the element
-	public static int[] removeTheElement(int[] arr, int index)
-	{
-
-		// If the array is empty
-		// or the index is not in array range
-		// return the original array
-		if (arr == null || index < 0
-			|| index >= arr.length) {
-
-			return arr;
-		}
-
-		// Create another array of size one less
-		int[] anotherArray = new int[arr.length - 1];
-
-		// Copy the elements except the index
-		// from original array to the other array
-		for (int i = 0, k = 0; i < arr.length; i++) {
-
-			// if the index is
-			// the removal element index
-			if (i == index) {
-				continue;
-			}
-
-			// if the index is not
-			// the removal element index
-			anotherArray[k++] = arr[i];
-		}
-
-		// return the resultant array
-		return anotherArray;
-	}
-
-	// Driver Code
-	public static void main(String[] args)
-	{
-
-		// Get the array
-		int[] arr = { 1, 2, 3, 4, 5 };
-
-		// Print the resultant array
-		System.out.println("Original Array: "
-						+ Arrays.toString(arr));
-
-		// Get the specific index
-		int index = 2;
-
-		// Print the index
-		System.out.println("Index to be removed: " + index);
-
-		// Remove the element
-		arr = removeTheElement(arr, index);
-
-		// Print the resultant array
-		System.out.println("Resultant Array: "
-						+ Arrays.toString(arr));
-	}
+  public TreeNode(int val) {
+      this.val = val;
+      this.left = this.right = null;
+  }
 }
