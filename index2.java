27755f15be3d95d6a53a7cf0f8162869b03d6d51
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-05-31 12:43:06 +0100

    task 153 completed

--- index2.java
+++ index2.java
@@ -1,35 +1,31 @@
 /*
-Write a Java program that accepts four integers from the user and prints equal if all four are equal, and not equal otherwise.
+Write a Java program that accepts two double variables and test if both strictly between 0 and 1 and false otherwise.
 
 Sample Output:
-Input first number: 25
-Input second number: 37
-Input third number: 45
-Input fourth number: 23
-Numbers are not equal!
+Input first number: 5
+Input second number: 1
+false
  */
 
 import java.util.Scanner;
-public class index2 {
-    public static void main(String[] args){
-        System.out.println((101 + 0) / 3); // 33
-       Scanner sc = new Scanner(System.in);
-       System.out.println("Please enter integer one");
-       int num1 = sc.nextInt();
-       System.out.println("Please enter integer two");
-       int num2 = sc.nextInt();
-       System.out.println("Please enter integer three");
-       int num3 = sc.nextInt();
-       System.out.println("Please enter integer four");
-       int num4 = sc.nextInt();
-       sc.close();
 
-       if(num1 == num2 && num2 == num3 && num3 == num4 ){
-          System.out.println("Numbers are equal");
-       } 
-      else {
-          System.out.println("Numbers are not equal");
-       }
+public class index2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter first number");
+        double num1 = sc.nextDouble();
+        System.out.println("Please enter second number");
+        double num2 = sc.nextDouble();
+
+        sc.close();
+
+        // if (num1 >= 0 && num1 <=1 && num2 >= 0 && num2 <=1) {
+        //     System.out.println("True");
+        // } else {
+        //     System.out.println("False");
+        // }
+        // better method
+        System.out.println(num1 >= 0 && num1 <=1 && num2 >= 0 && num2 <=1);
 
     }
 }
