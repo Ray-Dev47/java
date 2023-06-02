213597921dfb1c13e9c59a90d17ee6627098df73
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-06-02 12:06:53 +0100

    task 155 completed

--- index2.java
+++ index2.java
@@ -1,41 +1,65 @@
 /*
-Write a Java program to print the contents of a two-dimensional Boolean array where t represents true and f represents false.
+Write a Java program to print an array after changing the rows and columns of a two-dimensional array.
 
-Sample array:
-array = {{true, false, true},
-{false, true, false}};
-Expected Output :
-t f t
-f t f
+Original Array:
+10 20 30
+40 50 60
+After changing the rows and columns of the said array:10 40
+20 50
+30 60
  */
 
 
-// public class index2 {
-//     public static void main(String[] args) {
-        
-//         // boolean boolOne [][] = {{true, false, true},{false, true, false}};
-//         int[][] boolOne = {{ 1, 2, 3 }, { 4, 5, 6 }};
-//          for (int i = 0; i < boolOne.length; i++) {
-//              for (int j = 0; j < boolOne[i].length; j++) {
-//                 System.out.println( boolOne[i][j] + "  ");
-//              }
-//              System.out.println();
-//          }        
-//     }
-// }
-
 public class index2 {
+
     public static void main(String[] args) {
-       final Boolean[][] matrix = {{true, false, true},{false, true, false}};
-       for (int i = 0; i < matrix.length; i++) { //this equals to the row in our matrix.
-          for (int j = 0; j < matrix[i].length; j++) { //this equals to the column in each row.
-             if(matrix[i][j]){
-                System.out.print(" t ");
-             }else{
-                System.out.print(" f ");
-             }
+        int[][] bool_One = {{10, 20, 30},{40, 50, 60}};
+        System.out.println("original Array:");
+       for (int i = 0; i < bool_One.length; i++) { //this equals to the row in our matrix.
+          for (int j = 0; j < bool_One[i].length; j++) { //this equals to the column in each row.          
+                System.out.print( bool_One[i][j] + " ");
           }
-          System.out.println(); //change line on console as row comes to end in the matrix.
+          System.out.println(); 
        }
     }
  }
+
+
+// //  halfway
+
+// import java.util.Scanner;
+// public class Solution {
+// 	public static void main(String[] args) {
+// 		int[][] twodm = {
+// 				{10, 20, 30},
+// 				{40, 50, 60}
+// 		};
+// 		System.out.print("Original Array:\n");
+// 		print_array(twodm);
+// 		System.out.print("After changing the rows and columns of the said array:");
+// 		transpose(twodm);
+// 		}
+	
+// 	private static void transpose(int[][] twodm) {
+		
+// 		int[][] newtwodm = new int[twodm[0].length][twodm.length];
+		
+// 		for (int i = 0; i < twodm.length; i++) {
+// 			for (int j = 0; j < twodm[0].length; j++) {
+// 				newtwodm[j][i] = twodm[i][j];
+// 			}
+// 		}
+		
+// 		print_array(newtwodm);
+// 	}
+// 	private static void print_array(int[][] twodm) {
+// 		for (int i = 0; i < twodm.length; i++) {
+// 			for (int j = 0; j < twodm[0].length; j++) {
+// 				System.out.print(twodm[i][j] + " ");
+// 			}
+// 			System.out.println();
+// 		}
+	
+// 	}
+// }
+
