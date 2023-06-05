37cae1d74080fa4699956b390b7cd853f389a964
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-06-05 21:41:25 +0100

    task 161 completed

--- index2.java
+++ index2.java
@@ -1,64 +1,33 @@
 /*
-Write a Java program to find the k smallest elements in a given array. Elements in the array can be in any order.
+Write a Java program to find the kth smallest and largest element in a given array. Elements in the array can be in any order.
 
 Expected Output:
 Original Array:
 [1, 4, 17, 7, 25, 3, 100]
-3 largest elements of the said array are:
-4,3,1
+K'th smallest element of the said array:
+3
+K'th largest element of the said array:
+25
  */
 
 
-// Java program to find the first, second
-// and third minimum element in an array
-// import java.util.*;
 
-public class index2
-{
-	static void Print3Smallest(int array[], int n)
-	{
-			int firstmin = Integer.MAX_VALUE;
-			int secmin = Integer.MAX_VALUE;
-			int thirdmin = Integer.MAX_VALUE;
-			for (int i = 0; i < n; i++)
-			{
-				/* Check if current element is less than
-				firstmin, then update first, second and
-				third */
-				if (array[i] < firstmin)
-				{
-					thirdmin = secmin;
-					secmin = firstmin;
-					firstmin = array[i];
-				}
-		
-				/* Check if current element is less than
-				secmin then update second and third */
-				else if (array[i] < secmin)
-				{
-					thirdmin = secmin;
-					secmin = array[i];
-				}
-		
-				/* Check if current element is less than
-				then update third */
-				else if (array[i] < thirdmin)
-					thirdmin = array[i];
-			}
-		
-			System.out.println("3 largest elements of the said array are: " + firstmin + " " + secmin + " " + thirdmin );
-			
-	}
-	
-	// Driver code
+public class index2{
+    // Driver code
 	public static void main(String[] args)
 	{
-			int array[] = {4, 9, 1, 32, 12};
-			int n = array.length;
-			Print3Smallest(array, n);
+			int[] arr = {1, 4, 17, 7, 25, 3, 100};
+            int first = arr[0];
+            int last = arr[arr.length - 1];
+            System.out.println("smallest element of the said array: " + first);
+            System.out.println("largest element of the said array: " + last);
 	}
 	
+
+
 }
+
+	
 
 
 
