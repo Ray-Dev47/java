8afbc3ef8f1fd079bb3c3e75e3adadbf998b1fc0
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-06-05 19:30:33 +0100

    task 159 completed

--- index2.java
+++ index2.java
@@ -12,54 +12,43 @@ After changing the rows and columns of the said array:10 40
 
 public class index2 {
 
-    public static void main(String[] args) {
-        int[][] bool_One = {{10, 20, 30},{40, 50, 60}};
-        System.out.println("original Array:");
-       for (int i = 0; i < bool_One.length; i++) { //this equals to the row in our matrix.
-          for (int j = 0; j < bool_One[i].length; j++) { //this equals to the column in each row.          
-                System.out.print( bool_One[i][j] + " ");
-          }
-          System.out.println(); 
-       }
+         static void print3largest(int arr[], int arrSize) {
+       int firstArr, secArr, thirdArr;
+
+        if(arrSize < 3){
+            System.out.println("Not up to 3");  // at least 3
+            return;
+        }
+
+        thirdArr = firstArr = secArr = Integer.MIN_VALUE;
+        for (int j = 0; j < arrSize; j++) {
+              if(arr[j] > firstArr){
+                  thirdArr = secArr;
+                  secArr = firstArr;
+                  firstArr = arr[j];
+              }
+
+              else if (arr[j] > secArr) {
+                thirdArr = secArr;
+                secArr = arr[j];
+            }
+ 
+            else if (arr[j] > thirdArr)
+                thirdArr = arr[j];
+        }
+ 
+        System.out.println("Three largest elements are " + firstArr + " " + secArr + " " + thirdArr);
+        }
+
+        public static void main(String[] args)
+        {
+            int arr[] = { 12, 13, 1, 10, 34, 1 };
+            int n = arr.length;
+            print3largest(arr, n);
+        }
+
+   
     }
- }
+ 
 
-
-// //  halfway
-
-// import java.util.Scanner;
-// public class Solution {
-// 	public static void main(String[] args) {
-// 		int[][] twodm = {
-// 				{10, 20, 30},
-// 				{40, 50, 60}
-// 		};
-// 		System.out.print("Original Array:\n");
-// 		print_array(twodm);
-// 		System.out.print("After changing the rows and columns of the said array:");
-// 		transpose(twodm);
-// 		}
-	
-// 	private static void transpose(int[][] twodm) {
-		
-// 		int[][] newtwodm = new int[twodm[0].length][twodm.length];
-		
-// 		for (int i = 0; i < twodm.length; i++) {
-// 			for (int j = 0; j < twodm[0].length; j++) {
-// 				newtwodm[j][i] = twodm[i][j];
-// 			}
-// 		}
-		
-// 		print_array(newtwodm);
-// 	}
-// 	private static void print_array(int[][] twodm) {
-// 		for (int i = 0; i < twodm.length; i++) {
-// 			for (int j = 0; j < twodm[0].length; j++) {
-// 				System.out.print(twodm[i][j] + " ");
-// 			}
-// 			System.out.println();
-// 		}
-	
-// 	}
-// }
 
