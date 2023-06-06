ac4546b92d427f112feabb169235497c320d4a2d
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-06-06 10:23:38 +0100

    task 162 completed

--- index2.java
+++ index2.java
@@ -1,13 +1,13 @@
 /*
-Write a Java program to find the kth smallest and largest element in a given array. Elements in the array can be in any order.
+Write a Java program that finds numbers greater than the average of an array.
 
 Expected Output:
 Original Array:
 [1, 4, 17, 7, 25, 3, 100]
-K'th smallest element of the said array:
-3
-K'th largest element of the said array:
+The average of the said array is: 22.0
+The numbers in the said array that are greater than the average are:
 25
+100
  */
 
 
@@ -16,18 +16,31 @@ public class index2{
     // Driver code
 	public static void main(String[] args)
 	{
-			int[] arr = {1, 4, 17, 7, 25, 3, 100};
-            int first = arr[0];
-            int last = arr[arr.length - 1];
-            System.out.println("smallest element of the said array: " + first);
-            System.out.println("largest element of the said array: " + last);
+			double[] arr = {1, 4, 17, 7, 25, 3, 100};
+            
+            double sum = 0;
+         
+            //compute sum
+            int i=0;
+            while(i < arr.length) {
+                sum += arr[i];
+                i++;
+            }
+             
+            //compute average
+            double average = (sum / arr.length); 
+            System.out.println( "The average of the said array is: "+  average);
+
+            for (int j = 0; j < arr.length; j++) {
+                if(average < arr[j]){
+                    System.out.println("arrays greater than the average: " +  arr[j]);
+                }
+            }
 	}
 	
-
+   
 
 }
-
-	
 
 
 
