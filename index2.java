6a3c011984bab8c8457dbb2ee5d64dec6785bde6
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-06-08 20:35:40 +0100

    task-165 completed

--- index2.java
+++ index2.java
@@ -1,35 +1,23 @@
 /*
-Write a Java program to divide the two given integers using the subtraction operator.
+Write a Java program to move every positive number to the right and every negative number to the left of a given array of integers.
+
 Expected Output:
-Input the dividend: 625
-Input the divider: 25
-Result 25.0
+Original array: [-2, 3, 4, -1, -3, 1, 2, -4, 0]
+Result: [-4, -3, -2, -1, 0, 1, 2, 3, 4]
  */
 
- import java.util.Scanner;
- public class index2 {	
-     public static float divide_using_subtraction(int dividend, int divider) {
-     if (divider == 0) {
-        return 0;
-     }	   
-     float result = 0;
-     while (dividend > divider) {
-       dividend -= divider;
-       result++;
-     }
-     float decimalPart = (float) dividend / (float) divider;
-     result += decimalPart;
-     return result;
-   }	
-     
+import java.util.Arrays;
+
+public class index2 {	
      public static void main(String[] args) {
-  Scanner in = new Scanner(System.in);
-         System.out.print("Input the dividend: ");
-         int dividend = in.nextInt();
-         System.out.print("Input the divider: ");
-         int divider = in.nextInt();
-         in.close();
-         System.out.println("\nResult: " + divide_using_subtraction(dividend,divider));		
+             int[] arr = {-2, 3, 4, -1, -3, 1, 2, -4, 0}; 
+             
+                System.out.println("Original Array: " + Arrays.toString(arr) );
+                // in ascending order        
+                    Arrays.sort(arr);
+                    System.out.println("Result: " + Arrays.toString(arr));
+          
+
          }
          
  }
