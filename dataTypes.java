dfbce52960794ff14d264e08678c71b5897b0027
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-06-12 23:06:48 +0100

    task 306 completed

--- dataTypes.java
+++ dataTypes.java
@@ -8,18 +8,30 @@ Expected Output :
 3456789 minutes is approximately 6 years and 210 days
 */
 import java.util.Scanner;
-
 public class dataTypes {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Input the number of minutes: "); 
-        int num = scan.nextInt();
-        int years = num / 525600;
-        int day = num / 1440;
-        int remMinutes = day % 525600;
-        
-        System.out.println( num + " minutes is approximately " +  years + " years and "  + remMinutes +  " days");
-        scan.close();
+
+    public static void main(String[] Strings) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input the time zone offset to GMT: ");
+        long timeZoneChange = input.nextInt();
+
+        long totalMilliseconds = System.currentTimeMillis();
+
+        long totalSeconds = totalMilliseconds / 1000;
+
+        long currentSecond = totalSeconds % 60;
+
+        long totalMinutes = totalSeconds / 60;
+
+        long currentMinute = totalMinutes % 60;
+
+        long totalHours = totalMinutes / 60;
+
+        long currentHour = ((totalHours + timeZoneChange) % 24);
+
+        System.out.println("Current time is " + currentHour + ":" + currentMinute + ":" + currentSecond);
+        input.close();
     }
-    
 }
