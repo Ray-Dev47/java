52d2e0f887317fe563cd1ce31b8770cb0300ec6b
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-06-14 10:23:11 +0100

    task 309 completed'

--- dataTypes.java
+++ dataTypes.java
@@ -1,43 +1,24 @@
 
 /*
-Write a Java program to take the user for a distance (in meters) and the time taken (as three numbers: hours, minutes, seconds), and display the speed, in meters per second, kilometers per hour and miles per hour (hint: 1 mile = 1609 meters).
+Write a Java program that reads a number and displays the square, cube, and fourth power.
 
-Test Data
-Input distance in meters: 2500
-Input hour: 5
-Input minutes: 56
-Input seconds: 23
-Expected Output :
-Your speed in meters/second is 0.11691531
-Your speed in km/h is 0.42089513
-Your speed in miles/h is 0.26158804
+Expected Output:
+Square: .2f
+Cube: .2f
+Fourth power: 50625.00
 */
 import java.util.Scanner;
-public class dataTypes {
+public class dataTypes{
 
-    public static void main(String[] Strings) {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input the side length value: ");
+        double val = in.nextDouble();
+        in.close();
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Input distance in meters ");
-        double distance = sc.nextDouble();
-        System.out.print("Input hour");
-        double hour = sc.nextDouble();
-        System.out.print("Input minutes");
-        double minutes = sc.nextDouble();
-        System.out.print("Input seconds");
-        double seconds = sc.nextDouble();
-
-
-        double timeSeconds = (hour*3600) + (minutes*60) + seconds;
-		double  mps = distance / timeSeconds;
-		double  kph = ( distance/1000.0f ) / ( timeSeconds/3600.0f );
-        double  mph = kph / 1.609f;
-
-		System.out.println("Your speed in meters/second is "+mps);
-		System.out.println("Your speed in km/h is "+kph);
-		System.out.println("Your speed in miles/h is "+mph);
-        sc.close();
+        System.out.printf("Square: %12.2f\n", val * val);
+        System.out.printf("Cube: %14.2f\n", val * val * val);
+        System.out.printf("Fourth power: %6.2f\n", Math.pow(val, 4));
     }
-   
 }
