3652450b8f478d9aea4d5de7693994bc6a35050d
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-06-14 22:37:01 +0100

    task 313 completed

--- dataTypes.java
+++ dataTypes.java
@@ -7,25 +7,13 @@ public class dataTypes{
 
     public static void main(String[] args)
     {
-        Double dn1 = 0.000213456321d;
-        boolean d1f = Double.isFinite(dn1);
-        Double dn2 = dn1 / 0;
-        boolean d2f = Double.isFinite(dn2);
-        Double dn3 = Double.POSITIVE_INFINITY * 0;
-        boolean d3f = Double.isFinite(dn3);
-        System.out.println("\nFinite doubles\n--------------");
-        System.out.println("Is "+dn1 + " is finite? " + d1f);
-        System.out.println("Is "+dn2 + " (dn1/0) is finite? " + d2f);
-        System.out.println("Is "+dn3 + " is finite? " + d3f);
-		Float fn1 = 5.3f;
-        boolean f1f = Float.isFinite(fn1);
-        Float fn2 = fn1 / 0;
-        boolean f2f = Float.isFinite(fn2);
-        Float fn3 = 0f / 0f;
-        boolean f3f = Float.isFinite(fn3);
-        System.out.println("\n\nFinite floats\n-------------");
-        System.out.println("Is "+fn1 + " is finite? " + f1f);
-        System.out.println("Is "+fn2 + " (fn1/0) is finite? " + f2f);
-        System.out.println("Is "+fn3 + " is finite? " + f3f);
+        int in1 = Integer.MIN_VALUE;
+		int in2 = Integer.MAX_VALUE;
+		System.out.println("Signed integers: " + in1 + ", " + in2);
+		System.out.println("-----------------------------------------");
+		int compare_Signed_num = Integer.compare(in1, in2);
+		System.out.println("Result of comparing signed numbers: " + compare_Signed_num);
+		int compare_Unsigned_num = Integer.compareUnsigned(in1, in2);
+        System.out.println("Result of comparing unsigned numbers: " + compare_Unsigned_num);
     }
 }
