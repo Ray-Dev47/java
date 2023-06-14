2f86f7407ddb191d090181fc3a7fd72f69454d1e
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-06-14 21:42:55 +0100

    task completed

--- dataTypes.java
+++ dataTypes.java
@@ -2,7 +2,7 @@
 /*
 Write a Java program that accepts two integers from the user and prints the sum, the difference, the product, the average, the distance (the difference between the integers), the maximum (the largest of the two integers), and the minimum (the smallest of the two integers).
 
-Test Data
+Test Data: 
 Input 1st integer: 25
 Input 2nd integer: 5
 Expected Output :
