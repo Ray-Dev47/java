f2c387429bd9c86a8fadee6167ae70d6bb324221
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-06-19 20:05:01 +0100

    task 502 completed

--- javaEnumsType.java
+++ javaEnumsType.java
@@ -2,24 +2,19 @@
 Enums are data type that enables us to store variable in a predefined constants
 The variable must be equal to one of the values that have been predefined for it. Common examples include compass directions (values of NORTH, SOUTH, EAST, and WEST) and the days of the week. Because they are constants, the names of an enum type's fields are in uppercase letters
 
-
-Write a Java program to create an enum called "DaysOfWeek" representing the days of the week.
+Task 502
+Write a Java program to create an enum called "Weekend" with constants representing the days of the weekend.
 */ 
 
 
 public class javaEnumsType {
-    public enum DaysOfWeek{
-        MONDAY,
-        TUESDAY,
-        WEDNESAY,
-        THURSDAY,
-        FRIDAY,
+    public enum Weekend{
         SATURDAY,
         SUNDAY
         
     }
     public static void main (String[] args){
-        DaysOfWeek today =  DaysOfWeek.MONDAY;
-        System.out.println("Today is: " + today);
+        Weekend bestWeekend =  Weekend.SUNDAY;
+        System.out.println("Best weekend is: " + bestWeekend);
     }
 }
