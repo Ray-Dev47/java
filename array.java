8f555e9e7079c462f3e4c9e20f05f09b54697d91
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-06-20 20:15:43 +0100

    task-702-completed

--- array.java
+++ array.java
@@ -1,5 +1,5 @@
 /*
- *  calculate the average value of array elements
+ *  Write a Java program to sum values of an array.
  */
 
 import java.util.Arrays;
@@ -11,19 +11,13 @@ public class array {
         System.out.println("Original Array: " + Arrays.toString(arr1));
         Arrays.sort(arr1);
 
-        // Sum
+        System.out.println("Sum of arrays: " + Arrays.toString(arr1));
+
         int sum = 0;
+
         for (int i : arr1)
             sum += i;
-            System.out.println("Array sum" + sum);  // 43
-
-        // count the digits
-        int digitCount = 0;
-        for (int i = 0; i < arr1.length; i++) {
-             digitCount++;
-        }
-        System.out.println("count: " + digitCount);
-
-        System.out.println("Average is: " + sum/digitCount );
+            System.out.println(sum);
     }
+
 }
