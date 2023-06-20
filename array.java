f5629af55e99570792871bcac9210385c0e3abcf
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-06-20 19:01:03 +0100

    task 703 completed

--- array.java
+++ array.java
@@ -1,21 +1,23 @@
 /*
- * Java program to sort a numeric array and a string array
+ *  Write a Java program to sum values of an array.
  */
 
 import java.util.Arrays;
 
 public class array {
-    public static void main(String[] args ) {
+    public static void main(String[] args) {
         int[] arr1 = { 1, 6, 3, 8, 4, 8, 9, 4 };
-        String[] arr2 = { "Ray", "Mike", "Ene" };
+
         System.out.println("Original Array: " + Arrays.toString(arr1));
         Arrays.sort(arr1);
 
-        System.out.println("Sorted integer: " + Arrays.toString(arr1));
+        // System.out.println("Sum of arrays: " + Arrays.toString(arr1));
 
-        Arrays.sort(arr2);
-        System.out.println("Sorted Strings: " + Arrays.toString(arr2));
+        int sum = 0;
 
+        for (int i : arr1)
+            sum += i;
+            System.out.println(sum);  // 43
     }
 
 }
