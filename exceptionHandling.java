e6cd87e620ff40432e53c8deaff8a82274398277
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-06-20 17:04:49 +0100

    task 602 completed

--- exceptionHandling.java
+++ exceptionHandling.java
@@ -1,28 +1,33 @@
 
     /*
-Java Exceptions to handle errors:
-
-Java programming language uses exceptions to handle errors and other exceptional events. An exception is an event that occurs during the execution of a program that disrupts the normal flow of instructions.
-
-This section covers how to catch and handle exceptions. It includes try, catch, and finally block, as well as chained exceptions and logging exercises.
-
-1. Write a Java program that throws an exception and catch it using a try-catch block.
+Write a Java program to create a method that takes an integer as a parameter and throws an exception if the number is odd.
 */
 
 import java.util.Scanner;
 public class exceptionHandling {
+    // int nums;
+    // public int even(int x){
+    //     if(nums % 2 != 0){
+    //         return x;
+    //     }
+    //     return x;
+    // }
     public static void main(String[] args) {
         try {
             Scanner sc = new Scanner(System.in);
             System.out.println("Input an integer");
             int nums1 = sc.nextInt();
-            System.out.println("Input 2nd integer");
-            int nums2 = sc.nextInt();
             sc.close();
-            System.out.println("Sum is: " + nums1 + nums2);
+            if(nums1 % 2 == 0){
+                System.out.println("Even num entered is " + nums1);
+            } else{
+                throw new Exception();
+         }
+            
         } catch (Exception e) {
-            System.out.println("invalid data type entered" + e);   // Something went wrong.
+            System.out.println("odd number entered" + e);   // Something went wrong.
         }
+        
     }
 }
 
