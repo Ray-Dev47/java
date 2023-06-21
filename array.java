91f2e902f079feb45679f7eae9d80303b06805bb
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-06-21 13:13:25 +0100

    task 707 completed

--- array.java
+++ array.java
@@ -1,26 +1,22 @@
 /*
- * Write a Java program to find the index of an array element.
+ * Write a Java program to remove a specific element from an array.
  */
 
-// import java.util.Arrays;
+import java.util.Arrays;
 
 public class array {
     public static void main(String[] args) {
-        int [] nums = {7,4,5,7,8,4,3};
-        int targetElement = 8;
-        int index = -1;
-
-        int i = 0;
-        while(i < nums.length) {
-            if(nums[i] == targetElement) {
-                index = i;
-                break;
-            }
-            i++;
+        int [] arr = {7,5,7,8,4,3};
+        int[] newArr = new int[arr.length -1];
+        // elememt to be removed
+        int j = 4;
+        for (int i = 0, k = 0; i < arr.length; i++) {
+            if(i != j){
+                newArr[k] = arr[i];
+                k++;
+            }            
         }
-         
-        System.out.println("Index of "+targetElement+" is : "+index);
-
-        
+        System.out.println("Array before deletion" + Arrays.toString(arr));
+        System.out.println("Array after deletion" + Arrays.toString(newArr));
     }
 }
