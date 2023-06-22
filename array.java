d1ee7fd183f60ec4c9655cbf7dc461543e231c69
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-06-22 14:02:07 +0100

    task 714 completed

--- array.java
+++ array.java
@@ -2,19 +2,33 @@
  * Write a Java program to find duplicate values in an array of string values.
  */
 
-
 public class array {
 
     public static void main(String[] args) {
         String[] arr = { "Java", "JavaScript", "Python", "C", "Ruby", "Java" };
-        System.out.println("Duplicate elements in given array: ");  
-        //Searches for duplicate element  
-        for(int i = 0; i < arr.length; i++) {  
-            for(int j = i + 1; j < arr.length; j++) {  
-                if(arr[i] == arr[j])  
-                    System.out.println(arr[j]);  
-            }  
-        }  
+        String[] arr2 = { "C", "Xamarin", "Flutter", "Java", "C++", "GO" };
+        System.out.println("");
 
+        String common;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr2.length; j++) {
+                if (arr[i] == arr2[j]) {
+                    common = arr[i];
+                    System.out.println(common);
+                }
+            }
+
+        }
     }
 }
+
+// var common = []; // Array to contain common elements
+// for(var i=0 ; i<arr1.length ; ++i) {
+// for(var j=0 ; j<arr2.length ; ++j) {
+// if(arr1[i] == arr2[j]) { // If element is in both the arrays
+// common.push(arr1[i]); // Push to common array
+// }
+// }
+// }
+
+// return common;
