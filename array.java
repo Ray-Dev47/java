c4ca3254f4cb6931fe7ce1d0cdb85d9127e02a59
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-06-23 16:16:55 +0100

    task 716 completed

--- array.java
+++ array.java
@@ -2,33 +2,24 @@
  * Write a Java program to find common elements between two integer arrays.
  */
 
+// import java.util.Arrays;
+
 public class array {
 
     public static void main(String[] args) {
-        int [] arr = { 3,4,5,7,32,7,9 };
-       int [] arr2 = { 4,7,2,4,9,87,1 };
-        System.out.println("");
+        int[] arr = { 3, 4, 5, 7, 32, 7, 9, 4, 7, 2, 4, 9, 87, 1 };
+        int current = arr[0];
+        boolean found = false;
 
-        int common;
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr2.length; j++) {
-                if (arr[i] == arr2[j]) {
-                    common = arr[i];
-                    System.out.println(common);
-                }
+            if (current == arr[i] && !found) {
+                found = true;
+            } else if (current != arr[i]) {
+                System.out.print(" " + current);
+                current = arr[i];
+                found = false;
             }
-
         }
+        System.out.print(" " + current);  // 3 4 5 7 32 7 9 4 7 2 4 9 87 1
     }
 }
-
-// var common = []; // Array to contain common elements
-// for(var i=0 ; i<arr1.length ; ++i) {
-// for(var j=0 ; j<arr2.length ; ++j) {
-// if(arr1[i] == arr2[j]) { // If element is in both the arrays
-// common.push(arr1[i]); // Push to common array
-// }
-// }
-// }
-
-// return common;
