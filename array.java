3ea3ce94cbbb2e842dafd29881736c0aa070585f
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-06-24 16:08:47 +0100

    task 727 completed

--- array.java
+++ array.java
@@ -1,22 +1,28 @@
 /*
- * Write a Java program to test two arrays' equality.
+ *Write a Java program to find the number of even and odd integers in a given array of integers
  */
 
 // import java.util.Arrays;
 
-import java.util.Arrays;
+// import java.util.Arrays;
 
 public class array {
 
     public static void main(String[] args) {
-        int [] arr = {1,2,4,6};
-        int [] arr2 = {1,5,4,6};
-        boolean ans = Arrays.equals(arr,arr2);
-
-        if(ans == true){
-            System.out.println("equal");
-        } else{
-            System.out.println("Not Equal");
+        int [] arr = {1,2,4,6,7,8,9,10,11};
+        int evenCount = 0;
+        int oddCount = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(i % 2 == 0){
+                evenCount++;
+            }
         }
+        System.out.println("Total even number is " + evenCount);
+        for (int i = 0; i < arr.length; i++) {
+             if(i % 2 != 0){
+                oddCount++;
+             }
+        }
+         System.out.println("Total odd number is " + oddCount);
     }
 }
