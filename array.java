c34cb750bac0fd86a656a79c273810ceb4f41d26
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-06-24 21:24:15 +0100

    task 732 completed

--- array.java
+++ array.java
@@ -1,27 +1,28 @@
 /*
- * Write a Java program to check if an array of integers is without 0 and -1
+ * Write a Java program to check if an array of integers contains two specified elements 65 and 77.
  */
 
 // import java.util.Arrays;
 
 
 public class array {
-
     public static void main(String[] args) {
-        int [] arr = {2,4,6,7,8,9,10,11, -1};
+        int [] arr = {2,4,6,7,8,9,10,11, -1, 77};
+        int num1 = 65;
+        int num2 = 77;
         boolean isContain = true;
          for (int x : arr) {
                 // System.out.println(x);
-                if(x <= 0 || x < 0){
+                if(x == num1 || x == num2){
                     isContain = false;
                     break;
                 }
             }
         if(isContain == false){
-            System.out.println("Array contains either 0 and -1");
+            System.out.println("Array contains either 66 or 77");
         } 
         else{
-            System.out.println("Array does not contains 0 and -1");
+            System.out.println("Array does not contains 66 and 77");
         }
     }
 }
