e9d73d67a8edd37314f7bea7b2a02b9a55d81461
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-08-03 17:38:59 +0100

    task 4000 completed

---
+++ Animal.java
@@ -0,0 +1,45 @@
+/*
+In Core, Java Polymorphism is one of easy concept to understand. Polymorphism definition is that Poly means many and morphos means forms. It describes the feature of languages that allows the same word or symbol to be interpreted correctly in different situations based on the context. There are two types of Polymorphism available in Java. For example, in English, the verb “run” means different things if you use it with “a footrace,” a “business,” or “a computer.” You understand the meaning of “run” based on the other words used with it. Check out this article for more information.
+
+Write a Java program to create a base class Animal (Animal Family) with a method called Sound(). Create two subclasses Bird and Cat. Override the Sound() method in each subclass to make a specific sound for each animal.
+ */
+
+
+class Animal{
+     public void sound(){
+        System.out.println("Animal sound here");
+     }
+    }
+     class Bird extends Animal{
+         public void sound(){
+           System.out.println("Bird makes a chirping sound");
+         }
+     }
+
+     class Cat extends Animal{
+         public void sound(){
+           System.out.println("Cat makes a meow sound");
+         }
+     }
+
+
+// class Animal {
+// public void animalSound() {
+// System.out.println("Animal makes a sound here");
+// }
+// }
+
+// Example 2
+// class Dog extends Animal {
+// public void animalSound() {
+// System.out.println("Dog barks: Woof Woof!");
+// }
+// }
+
+// class Horse extends Animal {
+// public void animalSound() {
+// System.out.println("Horse neighs!!!");
+// }
+// }
+
+
--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 public class Main {
   public static void main(String[] args) {
-       Car car = new Car();
-       car.setCompanyName("Toyota");
-       car.setModelName("Camry");
-       car.setYear(2009);
-       
-       System.out.println("Company name : " + car.getCompanyName() + " modelname: " + car.getModelName() + " year: " + car.getMileage());
+       Animal animal = new Animal();
+       Animal bird = new Bird();
+       Animal cat = new Cat();
+
+       bird.sound();
+       cat.sound();
+       animal.sound();  
   }
 }
