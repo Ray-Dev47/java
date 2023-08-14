2eff9af4c578d7831e1c5b82585a8e563791d3e3
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-08-14 20:23:25 +0100

    task 6014 completed

--- CompareString.java
+++ CompareString.java
@@ -1,10 +1,10 @@
 /*
-Write a Java program to check whether two String objects contain the same data.
+Write a Java program to compare a given string to another string, ignoring case considerations.
 
 Sample Output:
 
 "Stephen Edwin King" equals "Walter Winchell"? false                                                          
-"Stephen Edwin King" equals "Mike Royko"? false
+"Stephen Edwin King" equals "stephen edwin king"? true 
  */
 
 public class CompareString {
@@ -12,9 +12,9 @@ public static void main(String[] args)
     {
        String str1 = "Stephen Edwin King";
        String str2 = "Walter Winchell";
-       String str3 = "Mike Royko";
+       String str3 = "stephen edwin king";
 
-       System.out.println(str1.equals(str2));
-       System.out.println(str1.equals(str3));
+       System.out.println(str1.equalsIgnoreCase(str2));
+       System.out.println(str1.equalsIgnoreCase(str3));
     }
 }
