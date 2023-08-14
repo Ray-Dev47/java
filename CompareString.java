98757409811865d2758ae8a77f4bed05e6b2eafa
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-08-14 18:54:55 +0100

    task 6009 completed

--- CompareString.java
+++ CompareString.java
@@ -1,19 +1,22 @@
 /*
-Write a Java program to test if a given string contains the specified sequence of char values.
+Write a Java program to compare a given string to the specified character sequence.
 
 Sample Output:
 
-Original String: PHP Exercises and Python Exercises                                                           
-Specified sequence of char values: and                                                                        
-true 
+Comparing example.com and example.com: true                                                                   
+Comparing Example.com and example.com: false
  */
 
 public class CompareString {
 public static void main(String[] args)
     {
-        String str1 = "PHP Exercises and Python Exercises";
+        String str1 = "example.com";
+        String str2 = "Example.com";
+
+        CharSequence cs = "example.com";
         
-        System.out.println(str1.contains("and"));
+        System.out.println("Comparing "+str1+" and "+cs+": " + str1.contentEquals(cs));
+      System.out.println("Comparing "+str2+" and "+cs+": " + str2.contentEquals(cs));
        
        
        
