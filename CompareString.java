3311c637473cd6d83e986cfc26637f90bf0b2bfa
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-08-14 19:05:30 +0100

    task 6010 completed

--- CompareString.java
+++ CompareString.java
@@ -1,22 +1,21 @@
 /*
-Write a Java program to compare a given string to the specified character sequence.
+Write a Java program to compare a given string to a specified string buffer.
 
 Sample Output:
 
 Comparing example.com and example.com: true                                                                   
-Comparing Example.com and example.com: false
+Comparing Example.com and example.com: false 
  */
 
 public class CompareString {
 public static void main(String[] args)
     {
-        String str1 = "example.com";
-        String str2 = "Example.com";
-
-        CharSequence cs = "example.com";
-        
-        System.out.println("Comparing "+str1+" and "+cs+": " + str1.contentEquals(cs));
-      System.out.println("Comparing "+str2+" and "+cs+": " + str2.contentEquals(cs));
+      String str1 = "example.com", str2 = "Example.com";
+      StringBuffer strbuf = new StringBuffer(str1);
+      
+      System.out.println("Comparing "+str1+" and "+strbuf+": " + str1.contentEquals(strbuf));
+      
+      System.out.println("Comparing "+str2+" and "+strbuf+": " + str2.contentEquals(strbuf));
        
        
        
