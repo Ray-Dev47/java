bec0fee31b182354002dd6c02ae3811f0f06ea18
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-08-19 12:12:26 +0100

    task 6350 completed

--- dateClass.java
+++ dateClass.java
@@ -1,17 +1,17 @@
 /*
-Write a Java program to get the minimum value of the year, month, week, and date from the current date of a default calendar.
+Write a Java program to get the current time in New York.
  */
 
 import java.util.Calendar;
 // import java.util.Date;
+import java.util.TimeZone;
 
 public class dateClass {
        public static void main(String[] args) {
-          Calendar calendar =  Calendar.getInstance();
-          System.out.println("The Minimum year is: " + calendar.getActualMinimum(Calendar.YEAR));
-          System.out.println("The Minimum year is: " + calendar.getActualMinimum(Calendar.MONTH));
-          System.out.println("The Minimum year is: " + calendar.getActualMinimum(Calendar.WEEK_OF_YEAR));
-          System.out.println("The Minimum year is: " + calendar.getActualMaximum(Calendar.DATE));
+          Calendar ny_time =  Calendar.getInstance();
+          ny_time.setTimeZone(TimeZone.getTimeZone("America/New_York"));
+		System.out.println("Current Time in New York: " + ny_time.get(Calendar.HOUR_OF_DAY) + ":"
+		+ ny_time.get(Calendar.MINUTE)+":"+ny_time.get(Calendar.SECOND));
           
     }
 }
