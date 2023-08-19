75a23362494ea2b02b466e8c116995214dca983d
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-08-19 12:02:45 +0100

    task 6350 completed

--- dateClass.java
+++ dateClass.java
@@ -1,5 +1,5 @@
 /*
-Write a Java program to get the maximum value of the year, month, week, and date from the current date of a default calendar.
+Write a Java program to get the minimum value of the year, month, week, and date from the current date of a default calendar.
  */
 
 import java.util.Calendar;
@@ -8,10 +8,10 @@ import java.util.Calendar;
 public class dateClass {
        public static void main(String[] args) {
           Calendar calendar =  Calendar.getInstance();
-          System.out.println("The maximum year is: " + calendar.getActualMaximum(Calendar.YEAR));
-          System.out.println("The maximum year is: " + calendar.getActualMaximum(Calendar.MONTH));
-          System.out.println("The maximum year is: " + calendar.getActualMaximum(Calendar.WEEK_OF_YEAR));
-          System.out.println("The maximum year is: " + calendar.getActualMaximum(Calendar.DAY_OF_YEAR));
+          System.out.println("The Minimum year is: " + calendar.getActualMinimum(Calendar.YEAR));
+          System.out.println("The Minimum year is: " + calendar.getActualMinimum(Calendar.MONTH));
+          System.out.println("The Minimum year is: " + calendar.getActualMinimum(Calendar.WEEK_OF_YEAR));
+          System.out.println("The Minimum year is: " + calendar.getActualMaximum(Calendar.DATE));
           
     }
 }
