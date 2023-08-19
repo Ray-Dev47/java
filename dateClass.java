12227381761d84b82f705da5f2b8373b291711ba
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-08-20 00:16:34 +0100

    task 6370 completed

--- dateClass.java
+++ dateClass.java
@@ -1,20 +1,16 @@
 /*
-Write a Java program to get the current date and time.
+Write a Java program to get the last day of the current month.
 */
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+
 import java.util.Calendar;
 // import java.util.Date;
 
 public class dateClass {
        public static void main(String[] args) {
-         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-Calendar cal = Calendar.getInstance();
-System.out.println(dateFormat.format(cal.getTime()));
-
-          
-          
+        
+        Calendar calendar = Calendar.getInstance();
+        System.out.println("last day of the current month: " + calendar.getActualMaximum(Calendar.DATE));
     }
 }
 
