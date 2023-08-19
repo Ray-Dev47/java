58df8fc46caeac33b76762e3e2231fa3be37d690
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-08-20 00:51:49 +0100

    task 6400 comleted

--- dateClass.java
+++ dateClass.java
@@ -5,26 +5,21 @@ Write a Java program to get the last day of the current month.
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.time.YearMonth;
+// import java.util.Calendar;
 // import java.util.Date;
 
 public class dateClass {
        public static void main(String[] args) {
         
-        Calendar c = Calendar.getInstance();
+       YearMonth ym = YearMonth.of(2016, 9);
 
-        // Set the calendar to monday of the current week
-        c.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-   
-           System.out.println();
-        // Print dates of the current week starting on Monday
-          DateFormat df = new SimpleDateFormat("EEE dd/MM/yyyy");
-          System.out.println(df.format(c.getTime()));
-           for (int i = 0; i <6; i++) {
-            c.add(Calendar.DATE, 1);
-              }
-           System.out.println(df.format(c.getTime()));
-           System.out.println();
+	 String firstDay = ym.atDay(1).getDayOfWeek().name();
+	 String lastDay = ym.atEndOfMonth().getDayOfWeek().name();
+	 System.out.println();
+	 System.out.println(firstDay);
+ 	 System.out.println(lastDay);
+	 System.out.println();
     }
 }
 
