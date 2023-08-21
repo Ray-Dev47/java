4c9f1d3b4195d4c136e69c933548ac7ca3e0d614
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-08-21 11:49:22 +0100

    task 6420 completed

--- dateClass.java
+++ dateClass.java
@@ -7,26 +7,22 @@ Write a Java program to get the number of days in a month.
 // import java.time.YearMonth;
 // import java.util.Calendar;
 // import java.util.Date;
-import java.time.YearMonth;
+import java.text.DateFormatSymbols;
+// import java.time.YearMonth;
+import java.util.Locale;
  
 public class dateClass {
  
     public static void main(String[] args) {
-        // Get number of days in given month of the year
-        int numberOfDaysInMonth1 = getNumberOfDaysInMonth(2023, 7);
-        System.out.println("Number of days in july 2023: "+numberOfDaysInMonth1);
- 
-        int numberOfDaysInMonth2 = getNumberOfDaysInMonth(2023, 9);
-        System.out.println("Number of days in Sept 2023: "+ numberOfDaysInMonth2);
+        DateFormatSymbols symbols = new DateFormatSymbols(new Locale("en"));
+// for the current Locale :
+//   DateFormatSymbols symbols = new DateFormatSymbols(); 
+String[] dayNames = symbols.getShortWeekdays();
+for (String s : dayNames) { 
+   System.out.print(s + " ");
+}
  
     }
- 
-    // Method to get number of days in month
-    public static int getNumberOfDaysInMonth(int year,int month)
-    {
-        YearMonth yearMonthObject = YearMonth.of(year, month);
-        int daysInMonth = yearMonthObject.lengthOfMonth();
-        return daysInMonth;
-    }
+
 }
 
