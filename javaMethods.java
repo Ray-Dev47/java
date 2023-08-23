aa1ae9272541c7156929b926ca406ebbfeeddb23
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-08-23 12:04:18 +0100

    task 6590 completed

--- javaMethods.java
+++ javaMethods.java
@@ -1,42 +1,36 @@
 /*
-Write Java methods to calculate triangle area.
+Write a Java method to display the current date and time.
 
 Expected Output:
 
-Input Side-1: 10                                                                               
-Input Side-2: 15                                                                               
-Input Side-3: 20                                                                              
-The area of the triangle is 72.6184377413890
+Current date and time: Wednesday January 25, 2017 7:47:43
 */
 
-import java.util.Scanner;
 
+// public class javaMethods {
+//   public static void main(String[] args)
+//     {
+//          System.out.println( getDateTime());
+//     }
+
+//    public static double getDateTime() {
+       
+//         return ;
+//     }
+// }
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+// import java.util.Date;
 
 public class javaMethods {
-  public static void main(String[] args)
-    {
-        Scanner in = new Scanner(System.in);
-         System.out.print("Input Side-1: ");
-         double side1 = in.nextDouble();
-         System.out.print("Input Side-2: ");
-         double side2 = in.nextDouble();
-         System.out.print("Input Side-3: ");
-         double side3 = in.nextDouble();
-         in.close();
-         System.out.println( is_Valid(side1, side2,side3) ?
-                "The area of the triangle is " + area_triangle(side1, side2, side3) : "Invalid triangle" );
-    }
+       public static void main(String[] args) {
+         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+Calendar cal = Calendar.getInstance();
+System.out.println(dateFormat.format(cal.getTime()));
 
-  public static boolean is_Valid(double side1, double side2, double side3) {
-        if( side1 + side2 > side3 &&
-                side2 + side3 > side1 &&
-                side1 + side3 > side2) return true;
-        else return false;
-    }
-   public static double area_triangle(double side1, double side2, double side3) {
-        double area = 0;
-        double s = (side1 + side2 + side3)/2;
-        area = Math.sqrt(s*(s - side1)*(s - side2)*(s - side3));
-        return area;
+          
+          
     }
 }
