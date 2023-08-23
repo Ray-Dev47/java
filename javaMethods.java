f8eed343375c896cb25e33150b6ba50d19503ce9
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-08-23 10:35:58 +0100

    task 6570 completed

--- javaMethods.java
+++ javaMethods.java
@@ -1,55 +1,72 @@
 /*
-Write a Java method to check whether a year (integer) entered by the user is a leap year or not.
+Write a Java method to check whether a string is a valid password.
+Password rules:
+A password must have at least ten characters.
+A password consists of only letters and digits.
+A password must contain at least two digits.
 
 Expected Output:
 
-Input a year: 2017                                                                        
-false
+1. A password must have at least eight characters.                                             
+2. A password consists of only letters and digits.                                         
+3. A password must contain at least two digits                                        
+Input a password (You are agreeing to the above Terms and Conditions.): abcd1234           
+Password is valid: abcd1234 
 */
 
 import java.util.Scanner;
-public class javaMethods {
- 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Input the year: ");
-        int year = in.nextInt();
-        in.close();
 
-        System.out.print("Inputed year: " + leap_year(year) +"\n");
+public class javaMethods {
+    
+public static final int PASSWORD_LENGTH = 8;
+
+public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print(
+                "1. A password must have at least eight characters.\n" +
+                "2. A password consists of only letters and digits.\n" +
+                "3. A password must contain at least two digits \n" +
+                "Input a password (You are agreeing to the above Terms and Conditions.): ");
+        String s = input.nextLine();
+        input.close();
+
+        if (is_Valid_Password(s)) {
+            System.out.println("Password is valid: " + s);
+        } else {
+            System.out.println("Not a valid password: " + s);
+        }
+
     }
 
-     static int leap_year(int num){
-         boolean leap = false;
+    public static boolean is_Valid_Password(String password) {
 
-        // if the year is divided by 4
-    if (num % 4 == 0) {
+        if (password.length() < PASSWORD_LENGTH) return false;
 
-        // if the year is century
-        if (num % 100 == 0) {
-  
-          // if year is divided by 400
-          // then it is a leap year
-          if (num % 400 == 0)
-            leap = true;
-          else
-            leap = false;
+        int charCount = 0;
+        int numCount = 0;
+        for (int i = 0; i < password.length(); i++) {
+
+            char ch = password.charAt(i);
+
+            if (is_Numeric(ch)) numCount++;
+            else if (is_Letter(ch)) charCount++;
+            else return false;
         }
-        
-        // if the year is not century
-        else
-          leap = true;
-      }
-      else
-      leap = false;
 
-    if (leap)
-      System.out.println(num + " is a leap year.");
-    else
-      System.out.println(num + " is not a leap year.");
-    return num;
+
+        return (charCount >= 2 && numCount >= 2);
+    }
+
+    public static boolean is_Letter(char ch) {
+        ch = Character.toUpperCase(ch);
+        return (ch >= 'A' && ch <= 'Z');
+    }
+
+
+    public static boolean is_Numeric(char ch) {
+
+        return (ch >= '0' && ch <= '9');
     }
 
 }
-
-
