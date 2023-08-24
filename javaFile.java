f1e234fdb11fe27c38e92e0563335c56f944f098
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-08-24 21:26:56 +0100

    task 7001 completed

--- javaFile.java
+++ javaFile.java
@@ -1,18 +1,28 @@
 /*
-Write a Java program to get a list of all file/directory names in the given directory.
-
+Write a Java program to get specific files with extensions from a specified folder.
 */
 
 
 import java.io.File;
+import java.io.FilenameFilter;
 // import java.util.Date;
 public class javaFile {
        public static void main(String[] args) {   
-          File file = new File("/home/career/");
-          String[] fileList = file.list();
-          for(String name:fileList){
-             System.out.println(name);
-          }
+        //   File file = new File("C:\\Users\\User\\Desktop");
+          File file = new File("C:\\Users\\User\\Desktop");
+          String[] list = file.list(new FilenameFilter() {
+            @Override
+             public boolean accept(File dir, String name) {
+              if(name.toLowerCase().endsWith(".txt")){
+                     return true;
+                 } else {
+                     return false;
+                 }
+             }
+         });
+         for(String f:list){
+             System.out.println(f);   // worked
+         }
     }
 }
 
