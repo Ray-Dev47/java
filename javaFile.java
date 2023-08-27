f2f339933d18021464a78f5f5af253764f14b68f
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-08-27 14:56:14 +0100

    task 7008 completed

--- javaFile.java
+++ javaFile.java
@@ -1,39 +1,24 @@
 /*
-Write a Java program to determine the last modified date of a file.
+Write a Java program to read input from the Java console.
 */
 
 
 // import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.attribute.BasicFileAttributes;
+
 // import java.util.Date;
-public class javaFile {
-       public static void main(String[] args) {   
-        //   File file = new File("C:\\Users\\User\\Desktop\\DOM Terminologies.txt");
-        //   File myFile = new File("C:\\Users\\User\\Desktop\\DOM Terminologies.txt");
-         
-        //      System.out.println(  myFile.lastModified());  // Yes, it is a directory
-           String fileName = "C:\\\\Users\\\\User\\\\Desktop\\\\DOM Terminologies.txt";
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-        try {
-
-            Path file = Paths.get(fileName);
-            BasicFileAttributes attr =
-                Files.readAttributes(file, BasicFileAttributes.class);
-
-            System.out.println("creationTime: " + attr.creationTime());
-            System.out.println("lastAccessTime: " + attr.lastAccessTime());
-            System.out.println("lastModifiedTime: " + attr.lastModifiedTime());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+ public class javaFile {
+  public static void main(String[] args) throws IOException
+  {
+    BufferedReader R = new BufferedReader(new InputStreamReader(System.in));
+    System.out.print("Input your name: ");
+    String name = R.readLine();
+    System.out.println("Your name is: " + name);
+  }
 }
-
 
 
 
