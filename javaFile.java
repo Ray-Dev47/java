288bde923972e91403b119f4190cda323476a084
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-08-27 15:55:57 +0100

    task 7009 completed

--- javaFile.java
+++ javaFile.java
@@ -1,22 +1,21 @@
 /*
-Write a Java program to read input from the Java console.
+Write a Java program to get the file size in bytes, KB, MB.
 */
 
 
-// import java.io.File;
+import java.io.File;
 
-// import java.util.Date;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
  public class javaFile {
-  public static void main(String[] args) throws IOException
+  public static void main(String[] args)
   {
-    BufferedReader R = new BufferedReader(new InputStreamReader(System.in));
-    System.out.print("Input your name: ");
-    String name = R.readLine();
-    System.out.println("Your name is: " + name);
+      File file = new File("C:\\Users\\User\\Desktop\\DOM Terminologies.txt");
+      long bytes = file.length();
+
+            // print the file size
+            System.out.println(bytes + " bytes");
+            System.out.println(bytes / 1024.0 + " kb");
+            System.out.println(bytes / 1024000.0 + " MB");
   }
 }
 
