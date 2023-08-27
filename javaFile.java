bf5e6d510400f03b3a17357f91615d47cbc759f0
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-08-27 14:22:12 +0100

    task 7007 completed

--- javaFile.java
+++ javaFile.java
@@ -1,19 +1,36 @@
 /*
-Write a Java program to check if the given pathname is a directory or a file.
+Write a Java program to determine the last modified date of a file.
 */
 
 
-import java.io.File;
+// import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
 // import java.util.Date;
 public class javaFile {
        public static void main(String[] args) {   
-        //   File file = new File("C:\\Users\\User\\Desktop\\Desktop\\DOM Terminologies.txt");
-          File myFile = new File("C:\\Users\\User");
-          if(myFile.isDirectory()){
-             System.out.println( " Yes, it is a directory");  // Yes, it is a directory
-          } else{
-             System.out.println("No it is a file");
-          }
+        //   File file = new File("C:\\Users\\User\\Desktop\\DOM Terminologies.txt");
+        //   File myFile = new File("C:\\Users\\User\\Desktop\\DOM Terminologies.txt");
+         
+        //      System.out.println(  myFile.lastModified());  // Yes, it is a directory
+           String fileName = "C:\\\\Users\\\\User\\\\Desktop\\\\DOM Terminologies.txt";
+
+        try {
+
+            Path file = Paths.get(fileName);
+            BasicFileAttributes attr =
+                Files.readAttributes(file, BasicFileAttributes.class);
+
+            System.out.println("creationTime: " + attr.creationTime());
+            System.out.println("lastAccessTime: " + attr.lastAccessTime());
+            System.out.println("lastModifiedTime: " + attr.lastModifiedTime());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
