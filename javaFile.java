150ebda3e51235b6c57b512e215e50adc51fa9d8
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-08-28 09:57:32 +0100

    task 8000 completed

--- javaFile.java
+++ javaFile.java
@@ -1,41 +1,26 @@
 /*
-Write a Java program to read the contents of a file into a byte array.
+Write a Java program to create an array list, add some colors (strings) and print out the collection.
 */
 
-// import java.io.File;
-// import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.util.List;
+import java.util.ArrayList;
+// import java.util.List;
 
-
- public class javaFile {
+public class javaFile {
   public static void main(String[] args)
   {
-    //   File file = new File("C:\\Users\\User\\Desktop\\DOM Terminologies.txt");
-    //   long bytes = file.length();
-    String file_name = "C:\\Users\\User\\Desktop\\DOM Terminologies.txt";
-    InputStream fins = null;
-        try {
-            fins = new FileInputStream(file_name);
-            byte file_content[] = new byte[2*1024];
-            int read_count = 0;
-            while((read_count = fins.read(file_content)) > 0){
-                System.out.println(new String(file_content, 0, read_count-1));
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try{
-                if(fins != null) fins.close();
-            } catch(Exception ex){
-                 
-            }
+                 List <String> colors = new ArrayList<String>();
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Orange");
+        colors.add("Black");
+
+
+
+        // Using enhanced for loop to print all for the Collection ArrayList
+        for(String n: colors){
+           System.out.println(n);  // works 
         }
-          
     }
  }
 
