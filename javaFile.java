29c1173d0414fbe39dc62c4d7f6c8969f30fb6e5
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-08-29 00:18:37 +0100

    task 8008 completed

--- javaFile.java
+++ javaFile.java
@@ -1,10 +1,11 @@
 /*
-Write a Java program to copy one array list into another.
+Write a Java program to shuffle elements in an array list.
 */
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Collections;
+
 
 public class javaFile {
   public static void main(String[] args)
@@ -22,37 +23,9 @@ public class javaFile {
         //    System.out.println(n);  //
         // }
        
-        //copy array list content into another array list
-        ArrayList<String> secondArrayList=new ArrayList<>();
-        secondArrayList.addAll(colorSet);
-        //print all the content of array list
-        Iterator<String> colorSet2 = secondArrayList.iterator();
-        while (colorSet2.hasNext()) {
-            System.out.println(colorSet2.next());
-        }
+        Collections.shuffle(colorSet);
+        System.out.println(colorSet);
 
-
-
-        //method 2 
-        /*
-         *  List<String> List1 = new ArrayList<String>();
-  List1.add("1");
-  List1.add("2");
-  List1.add("3");
-  List1.add("4");
-  System.out.println("List1: " + List1);
-  List<String> List2 = new ArrayList<String>();
-  List2.add("A");
-  List2.add("B");
-  List2.add("C");
-  List2.add("D");
-  System.out.println("List2: " + List2);
-  // Copy List2 to List1
-  Collections.copy(List1, List2);
-  System.out.println("Copy List to List2,\nAfter copy:");
-  System.out.println("List1: " + List1);
-  System.out.println("List2: " + List2);
-         */
     }
  }
 
