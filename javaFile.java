7f322168593941bca0fbd32c8643af3cf152ea54
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-08-28 10:59:24 +0100

    task 8005 completed

--- javaFile.java
+++ javaFile.java
@@ -1,10 +1,9 @@
 /*
-Write a Java program to remove the third element from an array list.
+Write a Java program to search for an element in an array list.
 */
 
 import java.util.List;
 import java.util.ArrayList;
-// import java.util.List;
 
 public class javaFile {
   public static void main(String[] args)
@@ -15,13 +14,16 @@ public class javaFile {
         colors.add("Orange");
         colors.add("Black");
         colors.add("Indigo");
+        colors.add("Black");
         
 
-        colors.remove(3);  //black removed
+        colors.indexOf("Black");  
+        System.out.println(colors.indexOf("Black"));// 3
+        System.out.println(colors.lastIndexOf("Black"));// 5
         // Using enhanced for loop to print all for the Collection ArrayList
-        for(String n: colors){
-           System.out.println(n);  //
-        }
+        // for(String n: colors){
+        //    System.out.println(n);  //
+        // }
     }
  }
 
