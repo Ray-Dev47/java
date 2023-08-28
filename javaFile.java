0613b6c8c0ac1d3d66aabfd0bb638b0987aed3cf
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-08-28 08:01:06 +0100

    task 7010 completed

--- javaFile.java
+++ javaFile.java
@@ -1,24 +1,43 @@
 /*
-Write a Java program to get the file size in bytes, KB, MB.
+Write a Java program to read the contents of a file into a byte array.
 */
 
-
-import java.io.File;
+// import java.io.File;
+// import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 
  public class javaFile {
   public static void main(String[] args)
   {
-      File file = new File("C:\\Users\\User\\Desktop\\DOM Terminologies.txt");
-      long bytes = file.length();
-
-            // print the file size
-            System.out.println(bytes + " bytes");
-            System.out.println(bytes / 1024.0 + " kb");
-            System.out.println(bytes / 1024000.0 + " MB");
-  }
-}
-
+    //   File file = new File("C:\\Users\\User\\Desktop\\DOM Terminologies.txt");
+    //   long bytes = file.length();
+    String file_name = "C:\\Users\\User\\Desktop\\DOM Terminologies.txt";
+    InputStream fins = null;
+        try {
+            fins = new FileInputStream(file_name);
+            byte file_content[] = new byte[2*1024];
+            int read_count = 0;
+            while((read_count = fins.read(file_content)) > 0){
+                System.out.println(new String(file_content, 0, read_count-1));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try{
+                if(fins != null) fins.close();
+            } catch(Exception ex){
+                 
+            }
+        }
+          
+    }
+ }
 
 
 
