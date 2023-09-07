3fc567e0054391da5a1935c830d697d25b522c9c
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-09-07 14:32:49 +0100

    task 8212 completed

--- javaFile.java
+++ javaFile.java
@@ -1,9 +1,8 @@
 /*
-Write a Java program to empty an hash set.
+ Write a Java program to test if a hash set is empty or not.
 */
 
 import java.util.HashSet;
-
 
 public class javaFile {
   public static void main(String[] args)
@@ -17,8 +16,13 @@ public class javaFile {
         colorSet.add("Black");
 
 
-        colorSet.clear();
-        System.out.println("hashset size or number of element " + colorSet);
+        if(colorSet.isEmpty()){
+          System.out.println("List is empty");
+      }else{
+          System.out.println("List is not empty");
+      }
+      // System.out.println(colorSet);  // []
+        // System.out.println("hashset size or number of element " + colorSet);
         // for(String n: colorSet){
         //    System.out.println(n);  //
         // }
