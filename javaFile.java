30f062461714fc15cffc4956b415b80451fe0b39
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-09-08 12:07:54 +0100

    task 8215 completed

--- javaFile.java
+++ javaFile.java
@@ -1,8 +1,10 @@
 /*
-Write a Java program to convert a hash set to an array.
+Write a Java program to convert a hash set to a treeset.
 */
 
 import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class javaFile {
   public static void main(String[] args)
@@ -15,14 +17,16 @@ public class javaFile {
         colorSet.add("Indigo");
         colorSet.add("Black");
        
-        System.out.println(colorSet);
-        String arr[] = new String[colorSet.size()];
-        
-        // toArray() method converts the set to array
-        colorSet.toArray(arr);
-        for(String n: colorSet){
-           System.out.println(n);  //
-        }
+        System.out.println("Hashset " + colorSet);
+      
+        // Treeset
+        Set<String> hashSetToTreeSet = new TreeSet<>(colorSet);
+
+    // Print the TreeSet
+       System.out.println("TreeSet: " + hashSetToTreeSet);  // serialized
+        // for(String n: colorSet){
+        //    System.out.println(n);  //
+        // }
     }
  }
 
