d2ed07b32e99416de907651f7fddd59771aac034
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-09-08 12:30:05 +0100

    task 8216 completed

--- javaFile.java
+++ javaFile.java
@@ -1,29 +1,52 @@
 /*
-Write a Java program to convert a hash set to a treeset.
+Write a Java program to find numbers less than 7 in a tree set.
 */
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Iterator;
 import java.util.TreeSet;
 
 public class javaFile {
   public static void main(String[] args)
   {
-        HashSet<String> colorSet = new HashSet<String>();
-        colorSet.add("Red");
-        colorSet.add("Blue");
-        colorSet.add("Orange");
-        colorSet.add("Black");
-        colorSet.add("Indigo");
-        colorSet.add("Black");
-       
-        System.out.println("Hashset " + colorSet);
+    TreeSet <Integer>tree_num = new TreeSet<Integer>();
+    TreeSet <Integer>treeheadset = new TreeSet<Integer>();
       
-        // Treeset
-        Set<String> hashSetToTreeSet = new TreeSet<>(colorSet);
+    // Add numbers in the tree
+    tree_num.add(1);
+    tree_num.add(2);
+    tree_num.add(3);
+    tree_num.add(5);
+    tree_num.add(6);
+    tree_num.add(7);
+    tree_num.add(8);
+    tree_num.add(9);
+    tree_num.add(10);
+      
+    // Find numbers less than 7
+  treeheadset = (TreeSet)tree_num.headSet(7);  
+ 
+    // create an iterator
+    Iterator iterator;
+    iterator = treeheadset.iterator();
+      
+    //Displaying the tree set data
+    System.out.println("Tree set data: ");     
+    while (iterator.hasNext()){
+    System.out.println(iterator.next() + " ");
+    }
+        // HashSet<String> colorSet = new HashSet<String>();
+        // colorSet.add("Red");
+        // colorSet.add("Blue");
+        // colorSet.add("Orange");
+        // colorSet.add("Black");
+        // colorSet.add("Indigo");
+        // colorSet.add("Black");
+       
+        // System.out.println("Hashset " + colorSet);
+      
+        // // Treeset
+        // Set<String> hashSetToTreeSet = new TreeSet<>(colorSet);
 
-    // Print the TreeSet
-       System.out.println("TreeSet: " + hashSetToTreeSet);  // serialized
         // for(String n: colorSet){
         //    System.out.println(n);  //
         // }
