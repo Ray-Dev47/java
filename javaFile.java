a05619faa54885c6696a61df2fcb3a2f8655d08c
Raymond Idengeli <raymond.idengeli@gmail.com>
2023-09-12 12:51:16 +0100

    task 8410 completed

--- javaFile.java
+++ javaFile.java
@@ -1,20 +1,32 @@
 /*
- Write a Java program to find a specified element in a given array of elements using Binary Search.
+Write a Java program to find a specified element in a given array of elements using Linear Search.
 */
-import java.util.Arrays;
 public class javaFile {
 
+  static int search(int arr[], int n, int x)
+  {
+      for (int i = 0; i < n; i++) {
+          // Return the index of the element if the element
+          // is found
+          if (arr[i] == x)
+              return i;
+      }
+
+      // return -1 if the element is not found
+      return -1;
+  }
+
 	public static void main(String[] args) {
-		int numbers[] = {5, 6, 7, 8, 9, 10};
-    int key = 8;
+    int[] arr = { 3, 4, 1, 7, 5 };
+    int n = arr.length;
+     
+    int x = 4;
 
-    int searchNum = Arrays.binarySearch(numbers, key);
-    if(searchNum == -1){
-       System.out.println("Not found");
-    }else{
-       System.out.println(searchNum); // 3
-    }
-
+    int index = search(arr, n, x);
+    if (index == -1)
+        System.out.println("Element is not present in the array");
+    else
+        System.out.println("Element found at position " + index);
 	}
 }
 
